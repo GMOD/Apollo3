8423d8fbdb7c85e9fea92f9ed4e27948e09efb2a
peterkxie <peterkxie@yahoo.com>
2021-04-20 18:24:25 -0700

    work on the coding editing form

--- src/ApolloFeatureDetail/components/CodingEditingTabDetail.tsx
+++ src/ApolloFeatureDetail/components/CodingEditingTabDetail.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@material-ui/core'
 import { DataGrid } from '@material-ui/data-grid'
 import { observer } from 'mobx-react'
-import React from 'react'
+import React, { useState } from 'react'
 import { AplInputProps, ApolloFeature } from '../ApolloFeatureDetail'
+import CodingModal from './CodingModal'
 
 interface CodingRow {
   type: string
@@ -16,11 +18,21 @@ const CodingEditingTabDetail = ({
   clickedFeature: ApolloFeature
   props: AplInputProps
 }) => {
+  const { model } = props
+  const [selected, setSelected] = useState<ApolloFeature | undefined>(undefined)
+  const [codingModalInfo, setCodingModalInfo] = useState({
+    open: false,
+    data: {},
+  })
+
+  const handleClose = () => {
+    setCodingModalInfo({ open: false, data: {} })
+  }
+
   const columns = [
     { field: 'type', headerName: 'Type' },
     { field: 'start', headerName: 'Start' },
     { field: 'length', headerName: 'Length' },
-    { field: 'feature', headerName: 'Feature', hide: true },
   ]
 
   const rows: CodingRow[] = clickedFeature.children.map(
@@ -31,23 +43,51 @@ const CodingEditingTabDetail = ({
       start: child.location.fmin + 1, // have this editable
       end: child.location.fmax, // have this editable
       length: child.location.fmax - child.location.fmin,
-      feature: child,
     }),
   )
 
-  // consider using data grid instead of table
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <div style={{ display: 'flex', height: '100%' }}>
-        <DataGrid
-          disableColumnMenu
-          hideFooterSelectedRowCount
-          pageSize={25}
-          rows={rows}
-          columns={columns}
-        />
+    <>
+      <div style={{ height: 400, width: '100%' }}>
+        <div style={{ display: 'flex', height: '100%' }}>
+          <DataGrid
+            disableColumnMenu
+            hideFooterSelectedRowCount
+            pageSize={25}
+            rows={rows}
+            columns={columns}
+            onRowClick={rowData => {
+              setSelected(clickedFeature.children[rowData.row.id as number])
+            }}
+          />
+        </div>
       </div>
-    </div>
+      <div style={{ margin: 5 }}>
+        <Button
+          color="secondary"
+          variant="contained"
+          disabled={!selected}
+          onClick={async () => {
+            setCodingModalInfo({
+              open: true,
+              data: {
+                selected,
+              },
+            })
+          }}
+        >
+          Edit
+        </Button>
+        {codingModalInfo.open && (
+          <CodingModal
+            handleClose={handleClose}
+            model={model}
+            clickedFeature={clickedFeature}
+            loadData={codingModalInfo.data}
+          />
+        )}
+      </div>
+    </>
   )
 }
 
---
+++ src/ApolloFeatureDetail/components/CodingModal.tsx
@@ -0,0 +1,175 @@
+import React, { useState } from 'react'
+import {
+  Dialog,
+  DialogTitle,
+  Divider,
+  makeStyles,
+  TextField,
+  Button,
+  IconButton,
+} from '@material-ui/core'
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'
+import CloseIcon from '@material-ui/icons/Close'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
+import { ApolloFeature } from '../ApolloFeatureDetail'
+
+const useStyles = makeStyles(theme => ({
+  main: {
+    textAlign: 'center',
+    margin: theme.spacing(2),
+    padding: theme.spacing(2),
+    borderWidth: 2,
+    borderRadius: 2,
+  },
+  buttons: {
+    margin: theme.spacing(2),
+    color: theme.palette.text.primary,
+  },
+  root: {
+    width: '100%',
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+}))
+
+export default function CodingModal({
+  handleClose,
+  model,
+  clickedFeature,
+  loadData,
+}: {
+  handleClose: () => void
+  model: any
+  clickedFeature: ApolloFeature
+  loadData: any
+}) {
+  const classes = useStyles()
+  const [strand, setStrand] = useState<string | null>(
+    `${loadData.selected.location.strand === 1 ? '+' : '-'}`,
+  )
+
+  const handleStrand = async (
+    event: React.MouseEvent<HTMLElement>,
+    newStrand: string | null,
+  ) => {
+    setStrand(newStrand)
+    // fetch to change strand
+  }
+
+  return (
+    <Dialog
+      open
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      data-testid="coding-editing-modal"
+    >
+      <DialogTitle id="alert-dialog-title">
+        Edit Length
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={handleClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <Divider />
+      <div className={classes.root}>
+        <form>
+          <div style={{ marginRight: 20 }}>
+            <TextField disabled placeholder="5' end" style={{ width: '20%' }} />{' '}
+            <IconButton
+              aria-label="lower"
+              disabled={loadData.selected.type.name !== 'exon'}
+              style={{ marginTop: 5 }}
+              onClick={() => {
+                /* sends a signal to ws to decrease fmin of child*/
+              }}
+            >
+              <ArrowBackIcon />
+            </IconButton>{' '}
+            <TextField
+              value={loadData.selected.location.fmin}
+              disabled={loadData.selected.type.name !== 'exon'}
+              style={{ width: '30%' }}
+              onChange={() => {
+                /* sends a signal to set fmax */
+              }}
+            />
+            <IconButton
+              aria-label="raise"
+              disabled={loadData.selected.type.name !== 'exon'}
+              style={{ marginTop: 5 }}
+              onClick={() => {
+                /* sends a signal to ws to increase fmin of child*/
+              }}
+            >
+              <ArrowForwardIcon />
+            </IconButton>{' '}
+            <br />
+            <TextField
+              disabled
+              placeholder="3' end"
+              style={{ width: '20%' }}
+            />{' '}
+            <IconButton
+              aria-label="lower"
+              disabled={loadData.selected.type.name !== 'exon'}
+              style={{ marginTop: 5 }}
+              onClick={() => {
+                /* sends a signal to ws to decrease fmax of child*/
+              }}
+            >
+              <ArrowBackIcon />
+            </IconButton>{' '}
+            <TextField
+              value={loadData.selected.location.fmax}
+              disabled={loadData.selected.type.name !== 'exon'}
+              style={{ width: '30%' }}
+              onChange={() => {
+                /* sends a signal to set fmax */
+              }}
+            />
+            <IconButton
+              aria-label="raise"
+              disabled={loadData.selected.type.name !== 'exon'}
+              style={{ marginTop: 5 }}
+              onClick={() => {
+                /* sends a signal to ws to increase fmax of child*/
+              }}
+            >
+              <ArrowForwardIcon />
+            </IconButton>{' '}
+            <br />
+            <TextField
+              disabled
+              placeholder="strand"
+              style={{ width: '20%' }}
+            />{' '}
+            <ToggleButtonGroup value={strand} onChange={handleStrand}>
+              <ToggleButton value="+">+</ToggleButton>
+              <ToggleButton value="-">-</ToggleButton>
+            </ToggleButtonGroup>
+          </div>
+        </form>
+      </div>
+      <div className={classes.buttons}>
+        <Button
+          variant="contained"
+          style={{ marginRight: 5 }}
+          onClick={() => {
+            handleClose()
+          }}
+        >
+          Close
+        </Button>
+      </div>
+    </Dialog>
+  )
+}
