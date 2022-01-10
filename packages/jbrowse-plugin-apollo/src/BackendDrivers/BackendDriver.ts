6c7d2dcf3f1e1e6f7954bfe1a30707b3fc49d002
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-01-09 20:43:40 -0700

    Add AnnotationFeature

--- packages/jbrowse-plugin-apollo/src/BackendDrivers/BackendDriver.ts
+++ packages/jbrowse-plugin-apollo/src/BackendDrivers/BackendDriver.ts
@@ -2,13 +2,14 @@ import { Region } from '@jbrowse/core/util'
 
 import { Change } from '../ChangeManager/Change'
 import { ValidationResultSet } from '../Validations/ValidationSet'
+import { AnnotationFeatureI } from './AnnotationFeatures'
 
 export abstract class BackendDriver {
   constructor(
     private clientStore: any, // TODO add client store
   ) {}
 
-  abstract getFeatures(region: Region): any // ApolloFeature snapshot in
+  abstract getFeatures(region: Region): AnnotationFeatureI
 
   loadFeatures(region: Region): void {
     const features = this.getFeatures(region)
