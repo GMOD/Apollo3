61f39bfccfc02ea9103b71928c99743e193ce55c
Kyosti Sutinen <kyosti@ebi.ac.uk>
2022-02-10 15:10:41 +0000

    Cannot read properties of undefined (reading changedIds) -exception

--- packages/apollo-collaboration-server/src/change/change.controller.ts
+++ packages/apollo-collaboration-server/src/change/change.controller.ts
@@ -25,7 +25,7 @@ import { ChangeService } from './change.service'
 export class ChangeController {
   private readonly logger = new Logger(ChangeController.name)
   constructor(private readonly changeService: ChangeService) {
-    changeRegistry.registerChange('LocationEndChange', LocationEndChange)
+    changeRegistry.registerChange('LocationEndChange', LocationEndChange) // Do this only once
   }
 
   // // THIS WORKS FINE
@@ -40,12 +40,8 @@ export class ChangeController {
   //   @UseGuards(JwtAuthGuard)
   @Post('/submitchange')
   submitChange(@Body() serializedChange: SerializedChange) {
-    this.logger.debug(`1=${serializedChange.typeName}`)
-    this.logger.debug(`2=${serializedChange.changedIds}`)
-    this.logger.debug(`3=${JSON.stringify(serializedChange.changes)}`)
-    // changeRegistry.registerChange('LocationEndChange', LocationEndChange)
     this.logger.debug(`serializedChange=${JSON.stringify(serializedChange)}`)
-    // const change = LocationEndChange.fromJSON(serializedChange)  // THIS CAUSES ERROR : TypeError: Cannot read properties of undefined (reading 'changedIds')
+    const change = LocationEndChange.fromJSON(serializedChange)  // THIS CAUSES ERROR : TypeError: Cannot read properties of undefined (reading 'changedIds')
 
     // Test version to test changeLocationEnd() -method
     const testChange = JSON.parse('{"changes":[{"featureId":"282f01b8-59e8-4595-83d9-751e0f1708c2","oldEnd":3300,"newEnd":4789}]}')
