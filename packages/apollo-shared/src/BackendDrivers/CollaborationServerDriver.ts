8694ff596bcf5b31bc67addb32e8c92cc7b94b80
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-02-24 23:52:17 +0000

    Use apollo_id for feature id

--- packages/apollo-shared/src/BackendDrivers/CollaborationServerDriver.ts
+++ packages/apollo-shared/src/BackendDrivers/CollaborationServerDriver.ts
@@ -57,14 +57,14 @@ function makeFeatures(gff3Contents: GFF3Item[], assemblyName: string) {
   > = {}
   for (const gff3Item of gff3Contents) {
     if (Array.isArray(gff3Item)) {
-      gff3Item.forEach((feature, idx) => {
+      gff3Item.forEach((feature) => {
         if (!feature.seq_id) {
           throw new Error('Got GFF3 record without an ID')
         }
         if (!feature.type) {
           throw new Error('Got GFF3 record without a type')
         }
-        const convertedFeature = convertFeature(feature, idx, assemblyName)
+        const convertedFeature = convertFeature(feature, assemblyName)
         const { refName } = convertedFeature.location
         let refRecord = featuresByRefName[refName]
         if (!refRecord) {
@@ -80,7 +80,6 @@ function makeFeatures(gff3Contents: GFF3Item[], assemblyName: string) {
 
 function convertFeature(
   feature: GFF3FeatureLineWithRefs,
-  idx: number,
   assemblyName: string,
 ): SnapshotIn<typeof AnnotationFeature> {
   if (!feature.seq_id) {
@@ -95,12 +94,14 @@ function convertFeature(
   if (!feature.end) {
     throw new Error('Got GFF3 record without an end')
   }
-  const attributeID = feature.attributes?.ID?.[0]
-  const id = attributeID ? `${attributeID}-${idx}` : objectHash(feature)
+  const id = feature.attributes?.apollo_id?.[0]
+  if (!id) {
+    throw new Error('Apollo feature without apollo_id encountered')
+  }
   const children: Record<string, SnapshotIn<typeof AnnotationFeature>> = {}
   feature.child_features.forEach((childFeatureLocation) => {
-    childFeatureLocation.forEach((childFeature, idx2) => {
-      const childFeat = convertFeature(childFeature, idx2, assemblyName)
+    childFeatureLocation.forEach((childFeature) => {
+      const childFeat = convertFeature(childFeature, assemblyName)
       children[childFeat.id] = childFeat
     })
   })
@@ -117,24 +118,4 @@ function convertFeature(
     newFeature.children = children
   }
   return newFeature
-}
-
-function hashCode(str: string): string {
-  let hash = 0
-  let i
-  let chr
-  if (str.length === 0) {
-    return '0'
-  }
-  for (i = 0; i < str.length; i++) {
-    chr = str.charCodeAt(i)
-    hash = (hash << 5) - hash + chr
-    hash |= 0 // Convert to 32bit integer
-  }
-  return String(hash)
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function objectHash(obj: Record<string, any>) {
-  return `${hashCode(JSON.stringify(obj))}`
 }
