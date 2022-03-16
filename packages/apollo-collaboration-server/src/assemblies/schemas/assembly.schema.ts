ecba3ad1aa123c3b00f9fbbc2132beb3a4111903
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-03-16 14:14:39 -0600

    Add initial schemas
    Co-authored-by: Kyosti Sutinen <kyosti@ebi.ac.uk>

--- packages/apollo-collaboration-server/src/assemblies/schemas/assembly.schema.ts
+++ packages/apollo-collaboration-server/src/assemblies/schemas/assembly.schema.ts
@@ -1,9 +1,19 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document } from 'mongoose'
+// import { Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document, Schema } from 'mongoose'
 
-export type AssemblyDocument = Assembly & Document
+// export type AssemblyDocument = Assembly & Document
 
-@Schema()
-export class Assembly {}
+// @Schema()
+// export class Assembly {}
 
-export const AssemblySchema = SchemaFactory.createForClass(Assembly)
+// export const AssemblySchema = SchemaFactory.createForClass(Assembly)
+
+// import { Document } from 'mongoose'
+
+export const AssemblySchema = new Schema({
+  description: { type: String, required: true },
+})
+export interface AssemblyDocument extends Document {
+  id: string
+  description: string
+}
--- packages/apollo-collaboration-server/src/features/schemas/feature.schema.ts
+++ packages/apollo-collaboration-server/src/features/schemas/feature.schema.ts
@@ -1,9 +1,22 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document } from 'mongoose'
+import { GFF3FeatureLineWithRefs } from '@gmod/gff'
+// import { Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document, Schema } from 'mongoose'
 
-export type FeatureDocument = Feature & Document
+// export type FeatureDocument = Feature & Document
 
-@Schema()
-export class Feature {}
+// @Schema()
+// export class Feature {}
 
-export const FeatureSchema = SchemaFactory.createForClass(Feature)
+// export const FeatureSchema = SchemaFactory.createForClass(Feature)
+
+export const FeatureSchema = new Schema({
+  refSeqId: { type: String, required: true },
+  featureId: { type: [String], required: true },
+  gff3FeatureLineWithRefs: { type: JSON, required: true },
+})
+export interface FeatureDocument extends GFF3FeatureLineWithRefs, Document {
+  id: string
+  refSeqId: string
+  featureId: string[]
+  gff3FeatureLineWithRefs: GFF3FeatureLineWithRefs
+}
