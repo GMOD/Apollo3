2a5af63fe7fc8c4e6250f3a3e0d44f84660bc435
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-04-06 02:42:57 +0000

    Synchronize structure with other modules

--- packages/apollo-collaboration-server/src/features/features.controller.spec.ts
+++ packages/apollo-collaboration-server/src/features/features.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
 import { FeaturesController } from './features.controller'
+import { FeaturesService } from './features.service'
 
 describe('FeaturesController', () => {
   let controller: FeaturesController
@@ -8,6 +9,7 @@ describe('FeaturesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FeaturesController],
+      providers: [FeaturesService],
     }).compile()
 
     controller = module.get<FeaturesController>(FeaturesController)
