1c70006b2eb2261bd7fef6c9193d3e3a46790705
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-04-19 12:40:16 -0600

    Handle all change types in the same service method

--- packages/apollo-collaboration-server/src/change/change.controller.ts
+++ packages/apollo-collaboration-server/src/change/change.controller.ts
@@ -1,12 +1,5 @@
-import {
-  Body,
-  Controller,
-  InternalServerErrorException,
-  Logger,
-  Post,
-  UseGuards,
-} from '@nestjs/common'
-import { SerializedChange, changeRegistry } from 'apollo-shared'
+import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common'
+import { SerializedChange } from 'apollo-shared'
 
 import { JwtAuthGuard } from '../utils/jwt-auth.guard'
 import { ChangeService } from './change.service'
@@ -21,25 +14,14 @@ export class ChangeController {
    * @param serializedChange - Information containing featureId, newEndValue, oldEndValue
    * @returns Return 'HttpStatus.OK' if featureId was found AND oldEndValue matched AND database update was successfull. Otherwise throw exception.
    */
-  //  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Post('/submitChange')
   async submitChange(@Body() serializedChange: SerializedChange) {
-    const ChangeType = changeRegistry.getChangeType(serializedChange.typeName)
-    const change = new ChangeType(serializedChange)
     this.logger.debug(
-      `Requested type: ${change.typeName}, the whole change: ${JSON.stringify(
-        change,
-      )}`,
+      `Requested type: ${
+        serializedChange.typeName
+      }, the whole change: ${JSON.stringify(serializedChange)}`,
     )
-    switch (change.typeName) {
-      case 'LocationStartChange':
-        return this.changeService.changeStartPos(serializedChange)
-      case 'LocationEndChange':
-        return this.changeService.changeEndPos(serializedChange)
-      default:
-        throw new InternalServerErrorException(
-          `Unknown change request type "${change.typeName}"`,
-        )
-    }
+    return this.changeService.submitChange(serializedChange)
   }
 }
--- packages/apollo-shared/src/ChangeManager/Change.ts
+++ packages/apollo-shared/src/ChangeManager/Change.ts
@@ -1,7 +1,6 @@
-import { GFF3FeatureLineWithRefs } from '@gmod/gff'
 import { FeatureDocument } from 'apollo-schemas'
+import { Cache } from 'cache-manager'
 import { IAnyStateTreeNode, Instance, SnapshotIn } from 'mobx-state-tree'
-import { Model } from 'mongoose'
 
 import { FeaturesForRefName } from '../BackendDrivers/AnnotationFeature'
 import { BackendDriver } from '../BackendDrivers/BackendDriver'
@@ -14,16 +13,14 @@ export interface ClientDataStore extends IAnyStateTreeNode {
   backendDriver?: BackendDriver
   internetAccountConfigId?: string
 }
-
 export interface LocalGFF3DataStore {
   typeName: 'LocalGFF3'
-  featureModel: Model<FeatureDocument>
+  cacheManager: Cache
+  gff3Handle: import('fs').promises.FileHandle
 }
-
-export interface GFF3FeatureLineWithRefsAndFeatureId
-  extends GFF3FeatureLineWithRefs {
-  featureId: string
-  GFF3FeatureLineWithRefs: GFF3FeatureLineWithRefs
+export interface ServerDataStore {
+  typeName: 'Server'
+  featureModel: import('mongoose').Model<FeatureDocument>
 }
 
 export interface SerializedChange extends Record<string, unknown> {
@@ -32,7 +29,7 @@ export interface SerializedChange extends Record<string, unknown> {
   typeName: string
 }
 
-export type DataStore = LocalGFF3DataStore | ClientDataStore
+export type DataStore = ServerDataStore | LocalGFF3DataStore | ClientDataStore
 
 export abstract class Change {
   /** have this return name of change type */
@@ -47,6 +44,9 @@ export abstract class Change {
 
   async apply(backend: DataStore): Promise<void> {
     const backendType = backend.typeName
+    if (backendType === 'Server') {
+      return this.applyToServer(backend)
+    }
     if (backendType === 'LocalGFF3') {
       return this.applyToLocalGFF3(backend)
     }
@@ -57,112 +57,10 @@ export abstract class Change {
       `no change implementation for backend type '${backendType}'`,
     )
   }
+
+  abstract applyToServer(backend: ServerDataStore): Promise<void>
   abstract applyToLocalGFF3(backend: LocalGFF3DataStore): Promise<void>
   abstract applyToClient(backend: ClientDataStore): Promise<void>
 
-  // ------------
-
-  // /**
-  //  * Get single feature by featureId
-  //  * @param featureObject -
-  //  * @param featureId -
-  //  * @returns
-  //  */
-  //  async getObjectByFeatureId(
-  //   entry: FeatureDocument,
-  //   featureId: string,
-  //   expectedOldEnd: number,
-  //   newEnd: number,
-  // ) {
-  //   if ('featureId' in entry) {
-  //     const assignedVal: GFF3FeatureLineWithRefsAndFeatureId =
-  //       Object.assign(entry)
-  //     // Check if there is also childFeatures in parent feature and it's not empty
-  //     if (
-  //       'child_features' in entry &&
-  //       Object.keys(entry.child_features).length > 0
-  //     ) {
-  //       // Let's get featureId from recursive method
-  //       console.debug(
-  //         `*** FeatureId was not found on top level so lets make recursive call...`,
-  //       )
-  //       const foundRecursiveObject = await this.getNestedFeatureByFeatureId(
-  //         assignedVal,
-  //         featureId,
-  //       )
-  //       if (foundRecursiveObject) {
-  //         if (foundRecursiveObject.end !== expectedOldEnd) {
-  //           const errMsg = `*** ERROR: Feature's current end value ${foundRecursiveObject.end} doesn't match with expected value ${expectedOldEnd}`
-  //           console.error(errMsg)
-  //           throw new Error(errMsg)
-  //         }
-  //         foundRecursiveObject.end = newEnd
-  //         return foundRecursiveObject
-  //       }
-  //     }
-  //   }
-  //   return null
-  // }
-
-  // /**
-  //  *
-  //  * @param parentFeature - parent feature where search will be started
-  //  * @param featureId - featureId to search
-  //  * @returns Found child feature, or return null if feature was not found
-  //  */
-  // async getNestedFeatureByFeatureId(
-  //   parentFeature: GFF3FeatureLineWithRefs,
-  //   featureId: string,
-  // ) {
-  //   // If there is child features and size is not 0
-  //   if (
-  //     'child_features' in parentFeature &&
-  //     Object.keys(parentFeature.child_features).length > 0
-  //   ) {
-  //     // Loop each child feature
-  //     for (
-  //       let i = 0;
-  //       i < Object.keys(parentFeature.child_features).length;
-  //       i++
-  //     ) {
-  //       // There can be several features with same ID so we need to loop
-  //       for (let j = 0; parentFeature.child_features[i].length > j; j++) {
-  //         const assignedVal: GFF3FeatureLineWithRefsAndFeatureId =
-  //           Object.assign(parentFeature.child_features[i][j])
-  //         // Let's add featureId if it doesn't exist yet
-  //         if ('featureId' in assignedVal) {
-  //           // If featureId matches
-  //           if (assignedVal.featureId === featureId) {
-  //             console.debug(
-  //               `*** Found featureId from recursive object: ${JSON.stringify(
-  //                 assignedVal,
-  //               )}`,
-  //             )
-  //             return assignedVal
-  //           }
-  //         }
-  //         // Check if there is also childFeatures in parent feature and it's not empty
-  //         if (
-  //           'child_features' in assignedVal &&
-  //           Object.keys(assignedVal.child_features).length > 0
-  //         ) {
-  //           // Let's add featureId to each child recursively
-  //           const foundObject = (await this.getNestedFeatureByFeatureId(
-  //             assignedVal,
-  //             featureId,
-  //           )) as GFF3FeatureLineWithRefs
-  //           console.debug(
-  //             `*** Found recursive object: ${JSON.stringify(foundObject)}`,
-  //           )
-  //           if (foundObject != null) {
-  //             return foundObject
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   return null
-  // }
-  // ------------
   abstract getInverse(): Change
 }
