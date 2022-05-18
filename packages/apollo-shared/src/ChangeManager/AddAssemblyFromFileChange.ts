264bbab396905b830c219738e4209d7816e75b68
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-05-18 02:14:49 +0000

    Use MongoDB session in change objects

--- packages/apollo-shared/src/ChangeManager/AddAssemblyFromFileChange.ts
+++ packages/apollo-shared/src/ChangeManager/AddAssemblyFromFileChange.ts
@@ -1,7 +1,7 @@
 import { join } from 'path'
 import { createGunzip } from 'zlib'
 
-import gff from '@gmod/gff'
+import gff, { GFF3Sequence } from '@gmod/gff'
 
 import {
   Change,
@@ -44,16 +44,24 @@ export class AddAssemblyFromFileChange extends Change {
     options?: ChangeOptions,
   ) {
     super(json, options)
-    this.changedIds = json.changedIds
     this.changes = 'changes' in json ? json.changes : [json]
   }
 
   toJSON() {
+    if (this.changes.length === 1) {
+      const [{ fileChecksum }] = this.changes
+      return {
+        typeName: this.typeName,
+        changedIds: this.changedIds,
+        assemblyId: this.assemblyId,
+        fileChecksum,
+      }
+    }
     return {
-      changedIds: this.changedIds,
       typeName: this.typeName,
-      changes: this.changes,
+      changedIds: this.changedIds,
       assemblyId: this.assemblyId,
+      changes: this.changes,
     }
   }
 
@@ -63,12 +71,9 @@ export class AddAssemblyFromFileChange extends Change {
    * @returns
    */
   async applyToServer(backend: ServerDataStore) {
-    const { refSeqModel, assemblyModel, refSeqChunkModel, fs } = backend
-    const { changes } = this
-    const { CHUNK_LEN } = process.env
-    if (!CHUNK_LEN) {
-      throw new Error('No CHUNK_LEN found in .env file')
-    }
+    const { assemblyModel, refSeqModel, refSeqChunkModel, fs, session } =
+      backend
+    const { changes, assemblyId } = this
 
     for (const change of changes) {
       const { fileChecksum, assemblyName } = change
@@ -78,93 +83,103 @@ export class AddAssemblyFromFileChange extends Change {
       if (!FILE_UPLOAD_FOLDER) {
         throw new Error('No FILE_UPLOAD_FOLDER found in .env file')
       }
-      const compressedFullFileName = join(FILE_UPLOAD_FOLDER, `${fileChecksum}`)
+      const compressedFullFileName = join(FILE_UPLOAD_FOLDER, fileChecksum)
 
       // Check and add new assembly
       const assemblyDoc = await assemblyModel
         .findOne({ name: assemblyName })
+        .session(session)
         .exec()
       if (assemblyDoc) {
         throw new Error(`Assembly "${assemblyName}" already exists`)
       }
       // Add assembly
-      const newAssemblyDoc = await assemblyModel.create({ name: assemblyName })
+      const [newAssemblyDoc] = await assemblyModel.create(
+        [{ _id: assemblyId, name: assemblyName }],
+        { session },
+      )
       this.logger.debug?.(
         `Added new assembly "${assemblyName}", docId "${newAssemblyDoc._id}"`,
       )
 
       // Read data from compressed file and parse the content
-      await fs
+
+      const sequenceStream = fs
         .createReadStream(compressedFullFileName)
         .pipe(createGunzip())
         .pipe(
           gff.parseStream({
-            parseFeatures: false,
-            parseDirectives: false,
-            parseComments: false,
             parseSequences: true,
+            parseComments: false,
+            parseDirectives: false,
+            parseFeatures: false,
           }),
         )
-        .on('data', async (data) => {
-          if (data.sequence) {
-            this.logger.debug?.(
-              `RefSeq: "${data.id}", length: ${data.sequence.length}`,
-            )
-            this.logger.debug?.(`RefSeq: "${data.id}"`)
-            const refSeqDoc = await refSeqModel
-              .findOne({ assembly: newAssemblyDoc._id, name: data.id })
-              .exec()
-            if (refSeqDoc) {
-              throw new Error(
-                `RefSeq "${data.id}" already exists in assemblyId "${newAssemblyDoc._id}"`,
-              )
-            }
-            // Add refSeq
-            const newRefSeqDoc = await refSeqModel.create({
-              name: data.id,
-              description: data.id,
+      for await (const s of sequenceStream) {
+        const sequence = s as GFF3Sequence
+        this.logger.debug?.(
+          `RefSeq: "${sequence.id}", length: ${sequence.sequence.length}`,
+        )
+        this.logger.debug?.(`RefSeq: "${sequence.id}"`)
+        const refSeqDoc = await refSeqModel
+          .findOne({ assembly: newAssemblyDoc._id, name: sequence.id })
+          .session(session)
+          .exec()
+        if (refSeqDoc) {
+          throw new Error(
+            `RefSeq "${sequence.id}" already exists in assemblyId "${newAssemblyDoc._id}"`,
+          )
+        }
+        // Add refSeq
+        const { CHUNK_SIZE } = process.env
+        const [newRefSeqDoc] = await refSeqModel.create(
+          [
+            {
+              name: sequence.id,
+              description: sequence.id,
               assembly: newAssemblyDoc._id,
-              length: data.sequence.length,
-            })
-            this.logger.debug?.(
-              `Added new refSeq "${data.id}", docId "${newRefSeqDoc._id}"`,
-            )
+              length: sequence.sequence.length,
+              ...(CHUNK_SIZE ? { chunkSize: Number(CHUNK_SIZE) } : null),
+            },
+          ],
+          { session },
+        )
+        this.logger.debug?.(
+          `Added new refSeq "${sequence.id}", docId "${newRefSeqDoc._id}"`,
+        )
 
-            let ind = 0
-            const chunkLength = Number(CHUNK_LEN)
-            const numChunks = Math.ceil(data.sequence.length / chunkLength)
-            for (let chunkNum = 0; chunkNum < numChunks - 1; chunkNum++) {
-              const start = chunkNum * chunkLength
-              const chunk = data.sequence.slice(start, start + chunkLength)
-              const newRefSeqChunkDoc = await refSeqChunkModel.create({
+        const { chunkSize } = newRefSeqDoc
+        const numChunks = Math.ceil(sequence.sequence.length / chunkSize)
+        for (let chunkNum = 0; chunkNum < numChunks; chunkNum++) {
+          const start = chunkNum * chunkSize
+          const chunk = sequence.sequence.slice(start, start + chunkSize)
+          await refSeqChunkModel.create(
+            [
+              {
                 refSeq: newRefSeqDoc._id,
                 n: chunkNum,
                 sequence: chunk,
-              })
-              this.logger.debug?.(
-                `Added new refSeqChunk (n=${ind}) docId "${newRefSeqChunkDoc._id}" for refSeq "${newRefSeqDoc._id}"`,
-              )
-              ind++
-            }
-          }
-        })
+                chunkSize,
+              },
+            ],
+            { session },
+          )
+        }
+      }
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  async applyToLocalGFF3(backend: LocalGFF3DataStore) {}
+  async applyToLocalGFF3(backend: LocalGFF3DataStore) {
+    throw new Error('applyToLocalGFF3 not implemented')
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   async applyToClient(dataStore: ClientDataStore) {}
 
   getInverse() {
+    const { changedIds, typeName, changes, assemblyId } = this
     return new AddAssemblyFromFileChange(
-      {
-        changedIds: this.changedIds,
-        typeName: 'AddAssemblyFromFileChange',
-        changes: this.changes,
-        assemblyId: this.assemblyId,
-      },
+      { changedIds, typeName, changes, assemblyId },
       { logger: this.logger },
     )
   }
