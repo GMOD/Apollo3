33f2d57c661efb53ea57edc2b425ca76aa76905d
Kyosti Sutinen <kyosti@ebi.ac.uk>
2022-06-09 09:26:39 +0100

    tmp commit

--- packages/jbrowse-plugin-apollo/src/components/ImportFeatures.tsx
+++ packages/jbrowse-plugin-apollo/src/components/ImportFeatures.tsx
@@ -1,3 +1,5 @@
+import { EDEADLK } from 'constants'
+
 import { AbstractSessionModel, AppRootModel } from '@jbrowse/core/util'
 import {
   Button,
@@ -10,7 +12,7 @@ import {
 } from '@material-ui/core'
 import axios from 'axios'
 import { getRoot } from 'mobx-state-tree'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { ApolloInternetAccountModel } from '../ApolloInternetAccount/model'
 import { ViewModel } from './stateModel'
@@ -34,51 +36,48 @@ export function ImportFeatures({ session, handleClose }: ImportFeaturesProps) {
   const [collection, setCollection] = React.useState<any[]>([])
 
   const options = [
-    { label: 'Fruit', value: 'fruit' },
-    { label: 'Vegetable', value: 'vegetable' },
-    { label: 'Meat', value: 'meat' },
+    { label: '', value: '' },
+    { label: 'hardcoded assembly name', value: '624a7e97d45d7745c2532b03' },
   ]
-  const [value, setValue] = React.useState('fruit')
+  const [value, setValue] = React.useState('')
 
-  // *** NEW FETCH STARTS *****
-  const uri = new URL('/assemblies', baseURL).href
-  const apolloFetch = apolloInternetAccount?.getFetcher({
-    locationType: 'UriLocation',
-    uri,
-  })
-  console.log(`Assemblies get starts...`)
-  if (apolloFetch) {
-    apolloFetch(uri, {
-      method: 'GET',
-    })
-      .then((response) => response.json())
-      //   .then((res) => setCollection(res))
-      .then((res) => {
-        console.log(JSON.stringify(res))
-        res.forEach((item: any) => {
-          console.log(`Id: ${item._id}`)
-          console.log(`Name: ${item.name}`)
-          options.push({ label: 'item._id2', value: 'item.name2' })
-          setCollection((oldArray) => [...oldArray, res])
-          console.log(`1collection "${collection}"`)
-        })
-      })
-    // .then((res) => this.setState({ collection: res }))
-  }
-  console.log(`Assemblies get done`)
-  console.log(`2collection "${collection}"`)
-  // *** NEW FETCH ENDS *****
-  options.push({ label: 'item._id3', value: 'item.name3' })
+  console.log(`getAssemblies() call starts...`)
+  getAssemblies()
+  console.log(`getAssemblies() call ended`)
 
-  function handleChangeFruit(e: React.ChangeEvent<HTMLSelectElement>) {
+  function handleChangeAssembly(e: React.ChangeEvent<HTMLSelectElement>) {
     setValue(e.target.value)
   }
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChangeFile(e: React.ChangeEvent<HTMLInputElement>) {
     if (!e.target.files) {
       return
     }
     setFile(e.target.files[0])
+  }
+
+  // Get assemblies
+  async function getAssemblies() {
+    const uri = new URL('/assemblies', baseURL).href
+    const apolloFetch = apolloInternetAccount?.getFetcher({
+      locationType: 'UriLocation',
+      uri,
+    })
+    if (apolloFetch) {
+      await apolloFetch(uri, {
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          console.log(JSON.stringify(res))
+          //   res.forEach((item: any) => {
+          //     // console.log(`Id: ${item._id}`)
+          //     // console.log(`Name: ${item.name}`)
+          //   })
+          options.push({ label: 'item._id21', value: 'item.name21' })
+        })
+    }
+    console.log(`getAssemblies() finished`)
   }
 
   async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -139,25 +138,31 @@ export function ImportFeatures({ session, handleClose }: ImportFeaturesProps) {
 
   return (
     <Dialog open maxWidth="xl" data-testid="login-apollo">
-      <DialogTitle>Import Features</DialogTitle>
+      <DialogTitle>Import Features from GFF3 file</DialogTitle>
       <form onSubmit={onSubmit}>
-        <div>
-          <label>
-            What do we eat?
-            <select value={value} onChange={handleChangeFruit}>
-              {options.map((option) => (
-                <option value={option.value}>{option.label}</option>
-              ))}
-            </select>
-          </label>
-          <p>We eat today {value}!</p>
-        </div>
+        {/* <div>
+          <h4>Select assembly</h4>
+          <select value={value} onChange={handleChangeAssembly}>
+            {options.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+          <p>Selected assembly is {value}!</p>
+        </div> */}
         <DialogContent style={{ display: 'flex', flexDirection: 'column' }}>
-          <DialogContentText>Enter new assembly info</DialogContentText>
-          <h4>Upload GFF3 or FASTA file</h4>
+        <h4>Select assembly</h4>
+          <select value={value} onChange={handleChangeAssembly}>
+            {options.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+          <p>Selected assembly is {value}!</p>
+
+          {/* <DialogContentText>Enter new assembly info</DialogContentText> */}
+          <h4>Upload GFF3 to load features</h4>
           {/* <input type="file" onChange={(e) => setFile(e.target.value)} /> */}
-          <input type="file" onChange={handleChange} />
-          <TextField
+          <input type="file" onChange={handleChangeFile} />
+          {/* <TextField
             autoFocus
             margin="dense"
             id="name"
@@ -175,7 +180,7 @@ export function ImportFeatures({ session, handleClose }: ImportFeaturesProps) {
             fullWidth
             variant="standard"
             // onChange={(e) => setAssemblyDesc(e.target.value)}
-          />
+          /> */}
         </DialogContent>
         <DialogActions>
           <Button variant="contained" color="primary" type="submit">
