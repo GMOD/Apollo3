b21a036cf0049182bc2d0fb95542bd5e0e7ed967
Kyösti Sutinen <88434919+kyostiebi@users.noreply.github.com>
2022-06-17 16:49:27 +0100

    Add UI for importing features (#105)
    * init commit
    
    * tmp
    
    * tmp commit
    
    * tmp commit
    
    * tmp commit
    
    * init commit, works started...
    
    * tmp commit
    
    * tmp commit
    
    * tmp commit
    
    * tmp commit
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * tmp
    
    * Ready for draft PR
    
    * ready for draft PR
    
    * ready for draft PR
    
    * error handling done and drop-down menu style changed
    
    * error handling done and drop-down menu style changed
    
    * tmp commit
    
    * tmp commit
    
    * AssemblyName is shown in notification. Improved error handling from backend to UI
    
    * Cleanup
    
    Co-authored-by: Garrett Stevens <stevens.garrett.j@gmail.com>

--- packages/apollo-collaboration-server/src/changes/changes.service.ts
+++ packages/apollo-collaboration-server/src/changes/changes.service.ts
@@ -92,16 +92,21 @@ export class ChangesService {
 
     let changeDocId
     await this.featureModel.db.transaction(async (session) => {
-      await change.apply({
-        typeName: 'Server',
-        featureModel: this.featureModel,
-        assemblyModel: this.assemblyModel,
-        refSeqModel: this.refSeqModel,
-        refSeqChunkModel: this.refSeqChunkModel,
-        fileModel: this.fileModel,
-        session,
-        fs,
-      })
+      try {
+        await change.apply({
+          typeName: 'Server',
+          featureModel: this.featureModel,
+          assemblyModel: this.assemblyModel,
+          refSeqModel: this.refSeqModel,
+          refSeqChunkModel: this.refSeqChunkModel,
+          fileModel: this.fileModel,
+          session,
+          fs,
+        })
+      } catch (e) {
+        throw new UnprocessableEntityException(String(e))
+      }
+
       // Add change information to change -collection
       this.logger.debug(`ChangeIds: ${change.changedIds}`)
       this.logger.debug(`AssemblyId: ${change.assemblyId}`)
--- packages/jbrowse-plugin-apollo/src/components/ImportFeatures.tsx
+++ packages/jbrowse-plugin-apollo/src/components/ImportFeatures.tsx
@@ -6,25 +6,25 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-  Radio,
-  RadioGroup,
-  TextField,
+  MenuItem,
+  Select,
 } from '@material-ui/core'
-import { ObjectID } from 'bson'
 import { getRoot } from 'mobx-state-tree'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { ApolloInternetAccountModel } from '../ApolloInternetAccount/model'
 
-interface AddAssemblyProps {
+interface ImportFeaturesProps {
   session: AbstractSessionModel
   handleClose(): void
 }
 
-export function AddAssembly({ session, handleClose }: AddAssemblyProps) {
+interface Collection {
+  _id: string
+  name: string
+}
+
+export function ImportFeatures({ session, handleClose }: ImportFeaturesProps) {
   const { internetAccounts } = getRoot(session) as AppRootModel
   const { notify } = session
   const apolloInternetAccount = internetAccounts.find(
@@ -35,9 +35,22 @@ export function AddAssembly({ session, handleClose }: AddAssemblyProps) {
   }
   const { baseURL } = apolloInternetAccount
   const [assemblyName, setAssemblyName] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
   const [file, setFile] = useState<File>()
-  const [fileType, setFileType] = useState('text/x-gff3')
+  const [collection, setCollection] = useState<Collection[]>([])
+  const [assemblyId, setAssemblyId] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  function handleChangeAssembly(
+    e: React.ChangeEvent<{
+      name?: string | undefined
+      value: unknown
+    }>,
+  ) {
+    setAssemblyId(e.target.value as string)
+    setAssemblyName(
+      collection.find((i) => i._id === e.target.value)?.name as string,
+    )
+  }
 
   function handleChangeFile(e: React.ChangeEvent<HTMLInputElement>) {
     if (!e.target.files) {
@@ -45,6 +58,49 @@ export function AddAssembly({ session, handleClose }: AddAssemblyProps) {
     }
     setFile(e.target.files[0])
   }
+
+  useEffect(() => {
+    async function getAssemblies() {
+      const uri = new URL('/assemblies', baseURL).href
+      const apolloFetch = apolloInternetAccount?.getFetcher({
+        locationType: 'UriLocation',
+        uri,
+      })
+      if (apolloFetch) {
+        const response = await apolloFetch(uri, {
+          method: 'GET',
+        })
+        if (!response.ok) {
+          let msg
+          try {
+            msg = await response.text()
+          } catch (e) {
+            msg = ''
+          }
+          setErrorMessage(
+            `Error when inserting new features (while uploading file) — ${
+              response.status
+            } (${response.statusText})${msg ? ` (${msg})` : ''}`,
+          )
+          return
+        }
+        const data = await response.json()
+        data.forEach((item: Collection) => {
+          setCollection((result) => [
+            ...result,
+            {
+              _id: item._id,
+              name: item.name,
+            },
+          ])
+        })
+      }
+    }
+    getAssemblies()
+    return () => {
+      setCollection([{ _id: '', name: '' }])
+    }
+  }, [apolloInternetAccount, baseURL])
 
   async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -59,7 +115,7 @@ export function AddAssembly({ session, handleClose }: AddAssemblyProps) {
     const formData = new FormData()
     formData.append('file', file)
     formData.append('fileName', file.name)
-    formData.append('type', fileType)
+    formData.append('type', 'text/x-gff3')
     const apolloFetchFile = apolloInternetAccount?.getFetcher({
       locationType: 'UriLocation',
       uri: url,
@@ -77,7 +133,7 @@ export function AddAssembly({ session, handleClose }: AddAssemblyProps) {
           msg = ''
         }
         setErrorMessage(
-          `Error when inserting new assembly (while uploading file) — ${
+          `Error when inserting new features (while uploading file) — ${
             res.status
           } (${res.statusText})${msg ? ` (${msg})` : ''}`,
         )
@@ -86,7 +142,7 @@ export function AddAssembly({ session, handleClose }: AddAssemblyProps) {
       fileChecksum = (await res.json()).checksum
     }
 
-    // Add assembly and refSeqs
+    // Add features
     const uri = new URL('/changes/submitChange', baseURL).href
     const apolloFetch = apolloInternetAccount?.getFetcher({
       locationType: 'UriLocation',
@@ -97,8 +153,8 @@ export function AddAssembly({ session, handleClose }: AddAssemblyProps) {
         method: 'POST',
         body: JSON.stringify({
           changedIds: ['1'],
-          typeName: 'AddAssemblyFromFileChange',
-          assemblyId: new ObjectID(),
+          typeName: 'AddFeaturesFromFileChange',
+          assemblyId,
           fileChecksum,
           assemblyName,
         }),
@@ -114,58 +170,45 @@ export function AddAssembly({ session, handleClose }: AddAssemblyProps) {
           msg = ''
         }
         setErrorMessage(
-          `Error when inserting new assembly — ${res.status} (${
+          `Error when inserting new features — ${res.status} (${
             res.statusText
           })${msg ? ` (${msg})` : ''}`,
         )
         return
       }
     }
-    notify(`Assembly "${assemblyName}" added successfully`, 'success')
+    notify(
+      `Features added to assembly "${assemblyName}" successfully`,
+      'success',
+    )
     handleClose()
     event.preventDefault()
   }
 
   return (
     <Dialog open maxWidth="xl" data-testid="login-apollo">
-      <DialogTitle>Add new assembly</DialogTitle>
+      <DialogTitle>Import Features from GFF3 file</DialogTitle>
       <form onSubmit={onSubmit}>
         <DialogContent style={{ display: 'flex', flexDirection: 'column' }}>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Assembly name"
-            type="TextField"
-            fullWidth
-            variant="outlined"
-            onChange={(e) => setAssemblyName(e.target.value)}
-          />
-          <FormControl>
-            <FormLabel>Select GFF3 or FASTA file</FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="text/x-gff3"
-              name="radio-buttons-group"
-              onChange={(e) => setFileType(e.target.value)}
-            >
-              <FormControlLabel
-                value="text/x-gff3"
-                control={<Radio />}
-                label="GFF3"
-              />
-              <FormControlLabel
-                value="text/x-fasta"
-                control={<Radio />}
-                label="FASTA"
-              />
-            </RadioGroup>
-          </FormControl>
+          <DialogContentText>Select assembly</DialogContentText>
+          <Select
+            labelId="label"
+            value={assemblyId}
+            onChange={handleChangeAssembly}
+          >
+            {collection.map((option) => (
+              <MenuItem key={option._id} value={option._id}>
+                {option.name}
+              </MenuItem>
+            ))}
+          </Select>
+          <p />
+          <DialogContentText>Upload GFF3 to load features</DialogContentText>
           <input type="file" onChange={handleChangeFile} />
         </DialogContent>
         <DialogActions>
           <Button
-            disabled={!(assemblyName && file)}
+            disabled={!(assemblyId && file)}
             variant="contained"
             color="primary"
             type="submit"
