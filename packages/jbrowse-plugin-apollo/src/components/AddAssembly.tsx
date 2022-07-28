8a77c82d387a99cc4e49c5010543f58fc24b4a12
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-07-28 03:47:46 +0000

    Use own objectId generator

--- packages/jbrowse-plugin-apollo/src/components/AddAssembly.tsx
+++ packages/jbrowse-plugin-apollo/src/components/AddAssembly.tsx
@@ -1,5 +1,3 @@
-import { Buffer } from 'buffer'
-
 import { AbstractSessionModel, AppRootModel } from '@jbrowse/core/util'
 import {
   Button,
@@ -17,6 +15,7 @@ import {
   RadioGroup,
   TextField,
 } from '@mui/material'
+import { generateObjectId } from 'apollo-shared'
 import { getRoot } from 'mobx-state-tree'
 import React, { useState } from 'react'
 
@@ -241,44 +240,4 @@ export function AddAssembly({ session, handleClose }: AddAssemblyProps) {
       ) : null}
     </Dialog>
   )
-}
-
-let index = Math.floor(Math.random() * 0xffffff)
-
-function getInc(): number {
-  return (index = (index + 1) % 0xffffff)
-}
-
-/**
- * Generate a 12 byte id buffer used in ObjectId's
- *
- * @param time - pass in a second based timestamp.
- */
-function generateObjectId(time?: number): Buffer {
-  if ('number' !== typeof time) {
-    time = Math.floor(Date.now() / 1000)
-  }
-
-  const inc = getInc()
-  const buffer = Buffer.alloc(12)
-
-  // 4-byte timestamp
-  buffer.writeUInt32BE(time, 0)
-
-  // // set PROCESS_UNIQUE if yet not initialized
-  // if (PROCESS_UNIQUE === null) {
-  //   PROCESS_UNIQUE = randomBytes(5)
-  // }
-
-  const PROCESS_UNIQUE = window.crypto.getRandomValues(Buffer.alloc(5))
-
-  // 5-byte process unique
-  ;[buffer[4], buffer[5], buffer[6], buffer[7], buffer[8]] = PROCESS_UNIQUE
-
-  // 3-byte counter
-  buffer[11] = inc & 0xff
-  buffer[10] = (inc >> 8) & 0xff
-  buffer[9] = (inc >> 16) & 0xff
-
-  return buffer
 }
