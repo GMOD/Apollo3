11d4f3a1b3389a850edc5f64dbc31e134284c6ac
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-08-31 20:47:10 +0000

    Remove unused data loading endpoint

--- packages/apollo-collaboration-server/src/features/features.controller.ts
+++ packages/apollo-collaboration-server/src/features/features.controller.ts
@@ -1,17 +1,12 @@
 import {
-  Body,
   Controller,
   Get,
   Logger,
   Param,
-  Post,
   Query,
   Response,
   StreamableFile,
-  UploadedFile,
-  UseInterceptors,
 } from '@nestjs/common'
-import { FileInterceptor } from '@nestjs/platform-express/multer'
 import { Response as ExpressResponse } from 'express'
 
 import { FeatureRangeSearchDto } from '../entity/gff3Object.dto'
@@ -21,23 +16,6 @@ import { FeaturesService } from './features.service'
 export class FeaturesController {
   constructor(private readonly featuresService: FeaturesService) {}
   private readonly logger = new Logger(FeaturesController.name)
-
-  /**
-   * Load GFF3 file into database.
-   * You can call this endpoint like: curl http://localhost:3999/features/importGFF3 -F 'file=\@./save_this_file.txt' -F 'assembly=assemblyId'
-   * @param file - File to save
-   * @returns Return status 'HttpStatus.OK' if save was successful
-   * or in case of error return throw exception
-   */
-  @Post('importGFF3')
-  @UseInterceptors(FileInterceptor('file'))
-  async importGFF3(
-    @UploadedFile() file: Express.Multer.File,
-    @Body() body: { assembly: string },
-  ) {
-    this.logger.debug(`Adding new features for assemblyId: ${body.assembly}`)
-    return this.featuresService.loadGFF3DataIntoDb(file, body.assembly)
-  }
 
   /**
    * Export GFF3 from database.
