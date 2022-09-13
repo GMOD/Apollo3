9500c0f1b9565883c026fa1a51bc9fc05326541e
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-09-13 21:10:28 +0000

    Update testing framework

---
+++ packages/jbrowse-plugin-apollo/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
+  automock: false,
+  setupFiles: ['./jestSetup.js'],
+}
