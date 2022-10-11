8c1d6fa3ae712ce7ad1867d877eded61d264bc61
Will Haese-Hill <wahhill@gmail.com>
2022-10-11 13:13:12 +0000

    editable roles and delete button

--- packages/jbrowse-plugin-apollo/src/components/ManageUsers.tsx
+++ packages/jbrowse-plugin-apollo/src/components/ManageUsers.tsx
@@ -1,4 +1,5 @@
 import { AbstractSessionModel } from '@jbrowse/core/util'
+import DeleteIcon from '@mui/icons-material/Delete'
 import {
   Button,
   Dialog,
@@ -6,8 +7,15 @@ import {
   DialogContent,
   DialogTitle,
 } from '@mui/material'
-import { DataGrid, GridColumns, GridToolbar } from '@mui/x-data-grid'
-import React from 'react'
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridColumns,
+  GridRowId,
+  GridRowParams,
+  GridToolbar,
+} from '@mui/x-data-grid'
+import React, { useCallback, useMemo, useState } from 'react'
 
 const users = [
   { id: 'user0123', email: '0123@demo.com', role: 'read-only' },
@@ -20,12 +28,46 @@ interface ManageUsersProps {
   handleClose(): void
 }
 
+type Row = typeof users[number]
+
 export function ManageUsers({ session, handleClose }: ManageUsersProps) {
-  const gridColumns: GridColumns = [
-    { field: 'id', headerName: 'User', width: 140 },
-    { field: 'email', headerName: 'Email', width: 160 },
-    { field: 'role', headerName: 'Role', width: 140 },
-  ]
+  const [rows, setRows] = useState<Row[]>(users)
+
+  const deleteUser = useCallback(
+    (id: GridRowId) => () => {
+      setTimeout(() => {
+        setRows((prevRows) => prevRows.filter((row) => row.id !== id))
+      })
+    },
+    [],
+  )
+
+  const gridColumns = useMemo<GridColumns<Row>>(
+    () => [
+      { field: 'id', headerName: 'User', width: 140 },
+      { field: 'email', headerName: 'Email', width: 160 },
+      {
+        field: 'role',
+        headerName: 'Role',
+        width: 140,
+        type: 'singleSelect',
+        valueOptions: ['read-only', 'user', 'admin'],
+        editable: true,
+      },
+      {
+        field: 'actions',
+        type: 'actions',
+        getActions: (params: GridRowParams) => [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            onClick={deleteUser(params.id)}
+            label="Delete"
+          />,
+        ],
+      },
+    ],
+    [deleteUser],
+  )
 
   return (
     <Dialog open maxWidth="xl" data-testid="login-apollo" fullScreen>
@@ -36,11 +78,12 @@ export function ManageUsers({ session, handleClose }: ManageUsersProps) {
           <DataGrid
             autoPageSize
             pagination
-            rows={users}
+            rows={rows}
             columns={gridColumns}
             getRowId={(row) => row.id}
             components={{ Toolbar: GridToolbar }}
             getRowHeight={() => 'auto'}
+            experimentalFeatures={{ newEditingApi: true }}
           />
         </div>
       </DialogContent>
