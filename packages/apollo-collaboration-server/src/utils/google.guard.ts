0f5dd8d58a64ce81324e815513c4dd335fd70395
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-10-27 03:56:53 +0000

    Use OAuth state to keep track of app URL

--- packages/apollo-collaboration-server/src/utils/google.guard.ts
+++ packages/apollo-collaboration-server/src/utils/google.guard.ts
@@ -1,5 +1,15 @@
-import { Injectable } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
+import { ExecutionContext, Injectable } from '@nestjs/common'
+import { AuthGuard, IAuthModuleOptions } from '@nestjs/passport'
 
 @Injectable()
-export class GoogleAuthGuard extends AuthGuard('google') {}
+export class GoogleAuthGuard extends AuthGuard('google') {
+  getAuthenticateOptions(
+    context: ExecutionContext,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ): IAuthModuleOptions<any> | undefined {
+    const [req] = context.getArgs()
+    const urlSearchParams = new URLSearchParams(req.originalUrl)
+    const appURL = urlSearchParams.get('state')
+    return { state: { appURL } }
+  }
+}
