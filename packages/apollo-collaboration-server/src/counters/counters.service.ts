336ec0752b0cbc77ba011b058c522bebbc83bc9f
Will Haese-Hill <wahhill@gmail.com>
2022-11-02 14:10:19 +0000

    counters classes

---
+++ packages/apollo-collaboration-server/src/counters/counters.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, Logger, NotFoundException } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Counter, CounterDocument } from 'apollo-schemas'
+import { Model } from 'mongoose'
+
+@Injectable()
+export class CountersService {
+  constructor(
+    @InjectModel(Counter.name)
+    private readonly counterModel: Model<CounterDocument>,
+  ) {}
+
+  private readonly logger = new Logger(CountersService.name)
+
+  async getNextSequenceValue(sequenceName: string): Promise<number> {
+    const sequenceDocument = await this.counterModel
+      .findOneAndUpdate({
+        filter: { _id: sequenceName },
+        update: { $inc: { sequenceValue: 1 } },
+        returnNewDocument: true,
+      })
+      .exec()
+    if (!sequenceDocument) {
+      const errMsg = `ERROR when getting next sequence value`
+      this.logger.error(errMsg)
+      throw new NotFoundException(errMsg)
+    }
+    return sequenceDocument.sequenceValue
+  }
+}
