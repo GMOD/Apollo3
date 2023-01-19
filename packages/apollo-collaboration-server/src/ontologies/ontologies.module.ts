bf760dc03fe9af02b02052b8d03796ed90a2b420
Kyosti Sutinen <kyosti@ebi.ac.uk>
2023-01-19 19:54:02 +0000

    Work on populating possible types in DetailsView

--- packages/apollo-collaboration-server/src/ontologies/ontologies.module.ts
+++ packages/apollo-collaboration-server/src/ontologies/ontologies.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { Node, NodeSchema } from 'apollo-schemas'
 
+import { FeaturesModule } from '../features/features.module'
 import { OntologiesController } from './ontologies.controller'
 import { OntologiesService } from './ontologies.service'
 
@@ -10,6 +11,7 @@ import { OntologiesService } from './ontologies.service'
   providers: [OntologiesService],
   imports: [
     MongooseModule.forFeature([{ name: Node.name, schema: NodeSchema }]),
+    FeaturesModule,
   ],
   exports: [MongooseModule],
 })
