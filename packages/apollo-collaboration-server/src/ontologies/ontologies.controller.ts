3cd35399c25223d2f3f90addd9546e509f87a631
Kyosti Sutinen <kyosti@ebi.ac.uk>
2023-02-03 10:41:23 +0000

    Query from OBO-JSON file is ready for testing. Now, SO query is implemented when add new child feature in LinearView

--- packages/apollo-collaboration-server/src/ontologies/ontologies.controller.ts
+++ packages/apollo-collaboration-server/src/ontologies/ontologies.controller.ts
@@ -23,50 +23,52 @@ export class OntologiesController {
   constructor(private readonly ontologiesService: OntologiesService) {}
   private readonly logger = new Logger(OntologiesController.name)
 
-  /**
-   * Get children's allowed feature types by parent type.
-   * @param parentType - string
-   * @returns Return 'HttpStatus.OK' and the allowed children feature types if search was successful
-   * or if search data was not found or in case of error throw exception
-   */
+  // /**
+  //  * Get children's allowed feature types by parent type. This endpoint gets data from Mongo.
+  //  * @param parentType - string
+  //  * @returns Return 'HttpStatus.OK' and the allowed children feature types if search was successful
+  //  * or if search data was not found or in case of error throw exception
+  //  */
   // @UseGuards(JwtAuthGuard)
   // @Validations(Role.ReadOnly)
-  @Get(':parentType')
-  getChildrenTypes(@Param('parentType') parentType: string) {
-    this.logger.debug(
-      `Get allowed children's feature types for parent type: "${parentType}"`,
-    )
-    return this.ontologiesService.findChildrenTypesByParentType(parentType)
-  }
+  // @Get(':parentType')
+  // getChildrenTypes(@Param('parentType') parentType: string) {
+  //   this.logger.debug(
+  //     `Get allowed children's feature types for parent type: "${parentType}"`,
+  //   )
+  //   return this.ontologiesService.findChildrenTypesByParentType(parentType)
+  // }
+
+  // /**
+  //  * Get all possible feature types for given feature.
+  //  * @param parentType - string
+  //  * @returns Return 'HttpStatus.OK' and the allowed feature types if search was successful
+  //  * or if search data was not found or in case of error throw exception
+  //  */
+  // @UseGuards(JwtAuthGuard)
+  // @Validations(Role.ReadOnly)
+  // @Get('/possibleTypes/:featureId')
+  // async getPossibleTypes(@Param('featureId') featureId: string) {
+  //   this.logger.debug(
+  //     `Get possible feature types for featureId: "${featureId}"`,
+  //   )
+  //   const eka = await this.ontologiesService.getPossibleFeatureTypes(featureId)
+  //   return eka
+  // }
 
   /**
-   * Get all possible feature types for given feature.
-   * @param parentType - string
-   * @returns Return 'HttpStatus.OK' and the allowed feature types if search was successful
+   * Get children's allowed feature types by parent type. Data is retrieved from OBO JSON file
+   * @param parentType - feature's parent type
+   * @returns Return 'HttpStatus.OK' and the allowed children feature types if search was successful
    * or if search data was not found or in case of error throw exception
    */
   @UseGuards(JwtAuthGuard)
   @Validations(Role.ReadOnly)
-  @Get('/possibleTypes/:featureId')
-  async getPossibleTypes(@Param('featureId') featureId: string) {
-    this.logger.debug(
-      `Get possible feature types for featureId: "${featureId}"`,
-    )
-    const eka = await this.ontologiesService.getPossibleFeatureTypes(featureId)
-    return eka
-  }
-
-  /**
-   * Get children's allowed feature types by parent type.
-   * @param parentType - string
-   * @returns Return 'HttpStatus.OK' and the allowed children feature types if search was successful
-   * or if search data was not found or in case of error throw exception
-   */
-  // @UseGuards(JwtAuthGuard)
-  // @Validations(Role.ReadOnly)
   @Get('/json/:parentType')
   getTypesFromJson(@Param('parentType') parentType: string) {
-    this.logger.debug(`Get possible children types (from OBO-JSON) by parent type: "${parentType}"`)
+    this.logger.debug(
+      `Get possible children types (from OBO-JSON) by parent type: "${parentType}"`,
+    )
     return this.ontologiesService.featureTypeFromJson(parentType)
   }
 
