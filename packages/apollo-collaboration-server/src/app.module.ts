a7022c761df7c76ad3e94a4723a46a5787d9db6f
Garrett Stevens <stevens.garrett.j@gmail.com>
2023-02-09 03:57:38 +0000

    Add Operations Module and Service

--- packages/apollo-collaboration-server/src/app.module.ts
+++ packages/apollo-collaboration-server/src/app.module.ts
@@ -6,9 +6,11 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { AssembliesModule } from './assemblies/assemblies.module'
 import { AuthenticationModule } from './authentication/authentication.module'
 import { ChangesModule } from './changes/changes.module'
+import { CountersModule } from './counters/counters.module'
 import { FeaturesModule } from './features/features.module'
 import { FilesModule } from './files/files.module'
 import { MessagesModule } from './messages/messages.module'
+import { OperationsModule } from './operations/operations.module'
 import { RefSeqChunksModule } from './refSeqChunks/refSeqChunks.module'
 import { RefSeqsModule } from './refSeqs/refSeqs.module'
 import { UsersModule } from './users/users.module'
@@ -38,6 +40,8 @@ const nodeEnv = process.env.NODE_ENV || 'production'
     FilesModule,
     UsersModule,
     MessagesModule,
+    OperationsModule,
+    CountersModule,
   ],
   providers: [
     { provide: APP_GUARD, useClass: JwtAuthGuard },
