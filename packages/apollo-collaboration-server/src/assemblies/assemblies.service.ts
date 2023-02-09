020e8ed34f0bf66141799cea8f69f66ea3bcc572
Garrett Stevens <stevens.garrett.j@gmail.com>
2023-02-09 03:57:49 +0000

    Add GetAssembliesOperation

--- packages/apollo-collaboration-server/src/assemblies/assemblies.service.ts
+++ packages/apollo-collaboration-server/src/assemblies/assemblies.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, Logger, NotFoundException } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Assembly, AssemblyDocument } from 'apollo-schemas'
+import { GetAssembliesOperation } from 'apollo-shared'
 import { Model } from 'mongoose'
 
+import { OperationsService } from '../operations/operations.service'
 import { CreateAssemblyDto } from './dto/create-assembly.dto'
 import { UpdateAssemblyDto } from './dto/update-assembly.dto'
 
@@ -11,6 +13,7 @@ export class AssembliesService {
   constructor(
     @InjectModel(Assembly.name)
     private readonly assemblyModel: Model<AssemblyDocument>,
+    private readonly operationsService: OperationsService,
   ) {}
 
   private readonly logger = new Logger(AssembliesService.name)
@@ -20,7 +23,9 @@ export class AssembliesService {
   }
 
   findAll() {
-    return this.assemblyModel.find({ status: 0 }).exec()
+    return this.operationsService.executeOperation<GetAssembliesOperation>({
+      typeName: 'GetAssembliesOperation',
+    })
   }
 
   async findOne(id: string) {
