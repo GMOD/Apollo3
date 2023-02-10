4fed543b782a3717aa0c3f367f533b224a548244
Kyosti Sutinen <kyosti@ebi.ac.uk>
2023-02-10 13:07:36 +0000

    Ready for test

--- packages/apollo-collaboration-server/src/changes/changes.service.ts
+++ packages/apollo-collaboration-server/src/changes/changes.service.ts
@@ -32,11 +32,11 @@ import {
 } from 'apollo-shared'
 import { FilterQuery, Model } from 'mongoose'
 
-import {  OntologiesService } from '../ontologies/ontologies.service'
 import { CountersService } from '../counters/counters.service'
 import { FilesService } from '../files/files.service'
 import { ChangeMessage } from '../messages/entities/message.entity'
 import { MessagesGateway } from '../messages/messages.gateway'
+import { OntologiesService } from '../ontologies/ontologies.service'
 import { FindChangeDto } from './dto/find-change.dto'
 
 export class ChangesService {
