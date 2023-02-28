c1bfb8193c5ecf25598d1267cba44ad1b1d14eb0
Garrett Stevens <stevens.garrett.j@gmail.com>
2023-02-28 23:55:14 +0000

    Add missing JWT_SECRET_FILE file read

--- packages/apollo-collaboration-server/src/utils/strategies/jwt.strategy.ts
+++ packages/apollo-collaboration-server/src/utils/strategies/jwt.strategy.ts
@@ -1,14 +1,29 @@
+import fs from 'fs'
+
 import { Injectable, Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
 import { DecodedJWT } from 'apollo-shared'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 
+interface JWTSecretConfig {
+  JWT_SECRET?: string
+  JWT_SECRET_FILE?: string
+}
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   private readonly logger = new Logger(JwtStrategy.name)
-  constructor(configService: ConfigService<{ JWT_SECRET: string }, true>) {
-    const jwtSecret = configService.get('JWT_SECRET', { infer: true })
+  constructor(configService: ConfigService<JWTSecretConfig, true>) {
+    let jwtSecret = configService.get('JWT_SECRET', { infer: true })
+    if (!jwtSecret) {
+      // We can use non-null assertion since joi already checks this for us
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const uriFile = configService.get('JWT_SECRET_FILE', {
+        infer: true,
+      })!
+      jwtSecret = fs.readFileSync(uriFile, 'utf-8').trim()
+    }
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
