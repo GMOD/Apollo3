a5996f8ddbd92e82ee86545430caddb649ab2c3a
Kyosti Sutinen <kyosti@ebi.ac.uk>
2023-04-25 13:56:17 +0100

    code cleanup done

--- packages/apollo-collaboration-server/src/ontologies/ontologies.controller.ts
+++ packages/apollo-collaboration-server/src/ontologies/ontologies.controller.ts
@@ -1,12 +1,6 @@
-import fs from 'fs'
-import path from 'path'
-
 import { Controller, Get, Logger, Param } from '@nestjs/common'
-import { OboJson } from 'apollo-common'
 
 import { Public } from '../utils/jwt-auth.guard'
-import { Role } from '../utils/role/role.enum'
-import { Validations } from '../utils/validation/validatation.decorator'
 import { OntologiesService } from './ontologies.service'
 
 @Public()
@@ -21,7 +15,6 @@ export class OntologiesController {
    * @returns Return 'HttpStatus.OK' and the allowed children feature types if search was successful
    * or if search data was not found or in case of error throw exception
    */
-  @Validations(Role.ReadOnly)
   @Get('/descendants/:featureType')
   getTypesFromJson(@Param('featureType') featureType: string) {
     this.logger.debug(
@@ -50,42 +43,5 @@ export class OntologiesController {
   @Get('/go/findByStr/:str')
   goFindByStr(@Param('str') str: string) {
     return this.ontologiesService.goFindByStr(str.toUpperCase())
-  }
-
-  /**
-   * Get children's allowed feature types by parent type. Data is retrieved from OBO JSON file
-   * @param parentType - feature's parent type
-   * @returns Return 'HttpStatus.OK' and the allowed children feature types if search was successful
-   * or if search data was not found or in case of error throw exception
-   */
-  @Get('/dummy/:featureType')
-  dummyFromJson(@Param('featureType') featureType: string) {
-    this.logger.debug(
-      `Get possible children types (from OBO-JSON) by parent type: "${featureType}"`,
-    )
-
-    const ontologyFile = path.join('..', '..', 'data', 'go.json')
-    try {
-      const ontologyLocation = path.resolve(__dirname, ontologyFile)
-      const ontologyText = fs.readFileSync(ontologyLocation, 'utf8')
-      const ontologyJson = JSON.parse(ontologyText) as OboJson
-      const ontology: OboJson = ontologyJson
-      // Iterate over the nodes and edges in the JSON file
-      for (const node of ontology.graphs[0].nodes) {
-        if (node.lbl === 'organelle transport along microtubule') {
-          this.logger.debug?.(
-            `*************** Parent type is "organelle transport along microtubule", OboJson node is "${node.id}"`,
-          )
-          // parentId = node.id
-          break
-        }
-      }
-    } catch (error) {
-      this.logger.error('Error loading ontology file')
-      throw error
-    }
-
-    return 'JUU'
-    // return this.ontologiesService.getDescendants(featureType)
   }
 }
