7f9d71e628246407f691a72403ff2b5af3d6ee3e
Garrett Stevens <stevens.garrett.j@gmail.com>
2023-06-05 22:44:10 +0000

    Log all errors

--- packages/apollo-collaboration-server/src/global-exceptions.filter.ts
+++ packages/apollo-collaboration-server/src/global-exceptions.filter.ts
@@ -1,25 +1,12 @@
-import {
-  ArgumentsHost,
-  Catch,
-  Logger,
-  UnprocessableEntityException,
-} from '@nestjs/common'
+import { ArgumentsHost, Catch, Logger } from '@nestjs/common'
 import { BaseExceptionFilter } from '@nestjs/core'
-import { Error } from 'mongoose'
 
 @Catch()
 export class GlobalExceptionsFilter extends BaseExceptionFilter {
   private readonly logger = new Logger(GlobalExceptionsFilter.name)
 
   catch(exception: unknown, host: ArgumentsHost) {
-    let newException = exception
-    if (
-      exception instanceof Error.CastError ||
-      exception instanceof Error.ValidationError
-    ) {
-      newException = new UnprocessableEntityException(exception.message)
-    }
-    this.logger.error(newException)
-    super.catch(newException, host)
+    this.logger.error(exception)
+    super.catch(exception, host)
   }
 }
