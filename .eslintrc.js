a890350f078ec7f50751ae453003474a32e21789
Garrett Stevens <stevens.garrett.j@gmail.com>
2023-07-18 20:07:38 -0600

    Add comments to eslintrc, update yarn bins

--- .eslintrc.js
+++ .eslintrc.js
@@ -134,23 +134,11 @@ module.exports = {
       settings: {
         // These settings are from eslint-plugin-react
         react: {
-          createClass: 'createReactClass', // Regex for Component Factory to use,
-          // default to "createReactClass"
-          pragma: 'React', // Pragma to use, default to "React"
-          fragment: 'Fragment', // Fragment to use (may be a property of <pragma>), default to "Fragment"
-          version: 'detect', // React version. "detect" automatically picks the version you have installed.
+          // React version. "detect" automatically picks the version you have installed.
           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
           // It will default to "latest" and warn if missing, and to "detect" in the future
-          flowVersion: '0.53', // Flow version
+          version: 'detect',
         },
-        propWrapperFunctions: [
-          // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-          'forbidExtraProps',
-          { property: 'freeze', object: 'Object' },
-          { property: 'myFavoriteWrapper' },
-          // for rules that check exact prop wrappers
-          { property: 'forbidExtraProps', exact: true },
-        ],
         componentWrapperFunctions: [
           // The name of any function used to wrap components, e.g. Mobx `observer` function. If this isn't set, components wrapped by these functions will be skipped.
           'observer', // `property`
@@ -158,18 +146,9 @@ module.exports = {
           { property: 'observer', object: 'Mobx' },
           { property: 'observer', object: '<pragma>' }, // sets `object` to whatever value `settings.react.pragma` is set to
         ],
-        formComponents: [
-          // Components used as alternatives to <form> for forms, eg. <Form endpoint={ url } />
-          'CustomForm',
-          { name: 'Form', formAttribute: 'endpoint' },
-        ],
-        linkComponents: [
-          // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
-          'Hyperlink',
-          { name: 'Link', linkAttribute: 'to' },
-        ],
       },
     },
+    // Lint non-src files (e.g. jest.config.js) using a separate tsconfig
     {
       files: ['./packages/jbrowse-plugin-apollo/*.js'],
       parserOptions: {
@@ -177,14 +156,17 @@ module.exports = {
       },
       env: { node: true },
     },
+    // Specify Node env for cypress testing files
     {
       files: ['./packages/jbrowse-plugin-apollo/cypress/**/*.js'],
       env: { node: true },
     },
+    // Specify Node env for apollo-collaboration-server/
     {
       files: ['./packages/apollo-collaboration-server/**/*.ts'],
       env: { node: true },
     },
+    // Don't enforce tsdoc syntax in JS files
     {
       files: ['**/*.js'],
       rules: {
