2b32d1a34779330d52e61f3db59ca6ee51d485c7
dariober <dario.beraldi@gmail.com>
2023-08-14 15:44:30 +0100

    Start adding some cypress tests

--- packages/jbrowse-plugin-apollo/cypress/e2e/apollo.cy.ts
+++ packages/jbrowse-plugin-apollo/cypress/e2e/apollo.cy.ts
@@ -1,3 +1,32 @@
+describe('Apollo', () => {
+  it('Can add assembly', () => {
+    /* This completes fine but it doesn't really add a new assembly in mongodb (!?) */
+    cy.fixture('tmp.json').then((config) => {
+      cy.visit(config.apollo_url)
+      cy.contains('Continue as Guest').click()
+      cy.reload()
+      cy.contains('Apollo').click()
+      cy.contains('Add Assembly').click()
+      cy.get('input[type="TextField"]').type('volvox3')
+      cy.get('[value="text/x-fasta"]').check()
+      cy.get('input[type="file"]').selectFile('test_data/volvox.2bit')
+      cy.contains('Submit').click()
+      cy.reload()
+    })
+  })
+
+  it('Can open assembly', () => {
+    /* This fails as it doesn't select any assembly */
+    cy.fixture('tmp.json').then((config) => {
+      cy.visit(config.apollo_url)
+      cy.contains('Continue as Guest').click()
+      cy.reload()
+      cy.get('input[data-testid="assembly-selector"]').type('volvox_del')
+    })
+  })
+})
+
+/*
 describe('My First Test', () => {
   it('visits JBrowse with Apollo', () => {
     // You can put JBrowse 2 into any session you want this way at the beginning
@@ -13,4 +42,4 @@ describe('My First Test', () => {
       cy.contains('Log in to Demo Server')
     })
   })
-})
+}) */
