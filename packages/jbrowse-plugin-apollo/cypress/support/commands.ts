3abaa758074ca6cad913af888e4759afb7ef3abd
dariober <dario.beraldi@gmail.com>
2023-08-22 14:43:26 +0100

    Add tests and refactor

--- packages/jbrowse-plugin-apollo/cypress/support/commands.ts
+++ packages/jbrowse-plugin-apollo/cypress/support/commands.ts
@@ -1,25 +1,57 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginAsGuest', (configJsonFile) => {
+  cy.fixture(configJsonFile).then((config) => {
+    cy.visit(config.apollo_url)
+  })
+  cy.contains('Continue as Guest').click()
+  cy.reload()
+})
+
+Cypress.Commands.add('addAssemblyFromGff', (assemblyName, fin) => {
+  cy.contains('Apollo').click()
+  cy.contains('Add Assembly').click()
+  cy.get('input[type="TextField"]').type(assemblyName)
+  cy.get('input[value="text/x-gff3"]').check()
+  cy.get('input[type="file"]').selectFile(fin)
+  cy.get('[data-testid="CheckBoxIcon"]')
+    .parent()
+    .children()
+    .get('input[type="checkbox"]')
+    .should('be.checked')
+  cy.intercept('/changes').as('changes')
+  cy.contains('Submit').click()
+  cy.wait('@changes').its('response.statusCode').should('match', /2../)
+  cy.reload()
+})
+
+Cypress.Commands.add('selectAssemblyToView', (assemblyName) => {
+  cy.contains('Select assembly to view', { timeout: 10000 })
+  cy.get('[data-testid="assembly-selector"]').parent().click()
+  cy.contains(assemblyName).parent().click()
+  cy.intercept('POST', '/users/userLocation').as('userLocation')
+  cy.contains('Open').click()
+  cy.wait('@userLocation')
+})
+
+Cypress.Commands.add('searchFeatures', (query) => {
+  cy.intercept('POST', '/users/userLocation').as('userLocation')
+  cy.get('input[placeholder="Search for location"]').type(`${query}{enter}`)
+  cy.wait('@userLocation')
+})
+
+Cypress.Commands.add(
+  'currentLocationEquals',
+  (contig, start, end, tolerance) => {
+    cy.get('input[placeholder="Search for location"]')
+      .invoke('val')
+      .as('currentLocation')
+
+    cy.get<string>('@currentLocation').then((currentLocation) => {
+      const [xcontig, s, e] = currentLocation.split(/:|\.\./)
+      const xstart: number = parseInt(s.replace(',', ''), 10)
+      const xend: number = parseInt(e.replace(',', ''), 10)
+      expect(xcontig).equal(contig)
+      expect(xstart).to.be.within(start - tolerance, end + tolerance)
+      expect(xend).to.be.within(end - tolerance, end + tolerance)
+    })
+  },
+)
