2155cb91541c913d3c5c58fef7d1747aeb6ffc2f
dariober <dario.beraldi@gmail.com>
2023-09-04 08:13:43 +0100

    Work in progress mostly on feature resizing

--- packages/jbrowse-plugin-apollo/cypress/support/commands.ts
+++ packages/jbrowse-plugin-apollo/cypress/support/commands.ts
@@ -2,14 +2,15 @@ Cypress.Commands.add('loginAsGuest', () => {
   cy.fixture('config.json').then((config) => {
     cy.visit(config.apollo_url)
   })
-  cy.contains('Continue as Guest', { timeout: 15000 }).click()
+  cy.contains('Continue as Guest', { timeout: 10_000 }).click()
+  cy.wait(2000)
   cy.reload()
 })
 
 Cypress.Commands.add('deleteAssemblies', () => {
   cy.exec(
     // Hardcode the test DB so we don't accidentally delete from unexpected databases
-    "mongosh mongodb://localhost:27017/apolloTestDb  --eval 'db.assemblies.deleteMany({})'",
+    "mongosh mongodb://localhost:27017/apolloTestDb  --eval 'db.assemblies.deleteMany({}); db.features.deleteMany({})'",
   ).then((result) => {
     cy.log(result.stdout)
     cy.log(result.stderr)
@@ -17,8 +18,10 @@ Cypress.Commands.add('deleteAssemblies', () => {
 })
 
 Cypress.Commands.add('addAssemblyFromGff', (assemblyName, fin) => {
-  cy.get('button[data-testid="dropDownMenuButton"]').contains('Apollo').click()
-  cy.contains('Add Assembly', { timeout: 15000 }).click()
+  cy.get('button[data-testid="dropDownMenuButton"]', { timeout: 10_000 })
+    .contains('Apollo')
+    .click({ force: true, timeout: 10_000 })
+  cy.contains('Add Assembly', { timeout: 10_000 }).click()
   cy.get('input[type="TextField"]').type(assemblyName)
   cy.get('input[value="text/x-gff3"]').check()
   cy.get('input[type="file"]').selectFile(fin)
@@ -31,11 +34,11 @@ Cypress.Commands.add('addAssemblyFromGff', (assemblyName, fin) => {
   cy.contains('Submit').click()
   cy.wait('@changes').its('response.statusCode').should('match', /2../)
   cy.reload()
-  cy.contains('Select assembly to view', { timeout: 10000 })
+  cy.contains('Select assembly to view', { timeout: 10_000 })
 })
 
 Cypress.Commands.add('selectAssemblyToView', (assemblyName) => {
-  cy.contains('Select assembly to view', { timeout: 10000 })
+  cy.contains('Select assembly to view', { timeout: 10_000 })
   cy.get('[data-testid="assembly-selector"]').parent().click()
   cy.contains(assemblyName).parent().click()
   cy.intercept('POST', '/users/userLocation').as('selectAssemblyToViewDone')
@@ -58,8 +61,8 @@ Cypress.Commands.add(
 
     cy.get<string>('@currentLocation').then((currentLocation) => {
       const [xcontig, s, e] = currentLocation.split(/:|\.\./)
-      const xstart: number = parseInt(s.replace(',', ''), 10)
-      const xend: number = parseInt(e.replace(',', ''), 10)
+      const xstart: number = Number.parseInt(s.replace(',', ''), 10)
+      const xend: number = Number.parseInt(e.replace(',', ''), 10)
       expect(xcontig).equal(contig)
       expect(xstart).to.be.within(start - tolerance, end + tolerance)
       expect(xend).to.be.within(end - tolerance, end + tolerance)
