835a010d2bbb82e635d330d56bd10840fbe49a22
Garrett Stevens <stevens.garrett.j@gmail.com>
2023-09-06 12:54:23 -0600

    Add coverage information to PRs (#271)
    * Add Codecov coverage upload
    
    * Add coverage from untested files

--- packages/jbrowse-plugin-apollo/jest.config.js
+++ packages/jbrowse-plugin-apollo/jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
   automock: false,
   setupFiles: ['./jestSetup.js', 'fake-indexeddb/auto'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
 }
