0e6b70ff56536ddae41fad5594aac9b465845cdc
Garrett Stevens <stevens.garrett.j@gmail.com>
2023-11-10 05:26:00 +0000

    Make middleware for feature schema updateMany

--- packages/apollo-collaboration-server/src/features/features.module.ts
+++ packages/apollo-collaboration-server/src/features/features.module.ts
@@ -1,6 +1,6 @@
 import { Module, forwardRef } from '@nestjs/common'
 import { MongooseModule, getConnectionToken } from '@nestjs/mongoose'
-import { Feature, FeatureSchema } from 'apollo-schemas'
+import { Feature, FeatureDocument, FeatureSchema } from 'apollo-schemas'
 import { Connection } from 'mongoose'
 import idValidator from 'mongoose-id-validator'
 
@@ -25,10 +25,47 @@ import { FeaturesService } from './features.service'
         name: Feature.name,
         useFactory: (connection: Connection, checksService: ChecksService) => {
           FeatureSchema.plugin(idValidator, { connection })
-          FeatureSchema.post('save', async (doc) => {
-            if (doc.allIds.length > 0 && doc.status === 0) {
+          const runChecksOnDocument = async (doc: FeatureDocument) => {
+            // @ts-expect-error ownerDocument does exist, TS just doesn't know it
+            if (doc.ownerDocument() === doc && doc.status === 0) {
               await checksService.clearChecksForFeature(doc)
               await checksService.checkFeature(doc)
+            }
+          }
+          FeatureSchema.post('save', runChecksOnDocument)
+          FeatureSchema.post('updateOne', runChecksOnDocument)
+          FeatureSchema.post('update', async function () {
+            const features = await this.model.find<FeatureDocument>(
+              this.getQuery(),
+            )
+            for (const feature of features) {
+              await runChecksOnDocument(feature)
+            }
+          })
+          FeatureSchema.post('findOneAndUpdate', async function () {
+            const features = await this.model.find<FeatureDocument>(
+              this.getQuery(),
+            )
+            for (const feature of features) {
+              await runChecksOnDocument(feature)
+            }
+          })
+          FeatureSchema.post('updateMany', async function () {
+            const query = this.getQuery()
+            if (query.$and && query.$and[0]) {
+              delete query.$and[0].status
+            }
+            const features = await this.model.find<FeatureDocument>(query)
+            for (const feature of features) {
+              await runChecksOnDocument(feature)
+            }
+          })
+          FeatureSchema.post('replaceOne', async function () {
+            const features = await this.model.find<FeatureDocument>(
+              this.getQuery(),
+            )
+            for (const feature of features) {
+              await runChecksOnDocument(feature)
             }
           })
           return FeatureSchema
