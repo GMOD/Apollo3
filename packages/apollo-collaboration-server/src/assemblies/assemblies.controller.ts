ce51aedcbbc9d6d3b743652ce3af2c62716e5d33
Garrett Stevens <stevens.garrett.j@gmail.com>
2023-11-15 06:00:41 +0000

    Fix auth on assembly checks endpoint

--- packages/apollo-collaboration-server/src/assemblies/assemblies.controller.ts
+++ packages/apollo-collaboration-server/src/assemblies/assemblies.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Logger, Param, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Head,
+  Logger,
+  Param,
+  Post,
+} from '@nestjs/common'
 
 import { Role } from '../utils/role/role.enum'
 import { Validations } from '../utils/validation/validatation.decorator'
@@ -15,14 +23,9 @@ export class AssembliesController {
   constructor(private readonly assembliesService: AssembliesService) {}
   private readonly logger = new Logger(AssembliesController.name)
 
-  @Get()
-  findAll() {
-    return this.assembliesService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.assembliesService.findOne(id)
+  @Head('checks')
+  checksHead() {
+    return ''
   }
 
   @Post('checks')
@@ -31,5 +34,15 @@ export class AssembliesController {
       updatedChecks._id,
       updatedChecks.checks,
     )
+  }
+
+  @Get()
+  findAll() {
+    return this.assembliesService.findAll()
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.assembliesService.findOne(id)
   }
 }
