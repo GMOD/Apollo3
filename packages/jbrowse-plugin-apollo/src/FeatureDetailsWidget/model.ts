06a0b472b6b331ed9d3ade123a35b3832472f7ca
Garrett Stevens <stevens.garrett.j@gmail.com>
2024-02-13 07:13:57 +0000

    Make widget show feature after reload

--- packages/jbrowse-plugin-apollo/src/FeatureDetailsWidget/model.ts
+++ packages/jbrowse-plugin-apollo/src/FeatureDetailsWidget/model.ts
@@ -1,17 +1,60 @@
+import { getSession } from '@jbrowse/core/util'
 import { ElementId } from '@jbrowse/core/util/types/mst'
-import { AnnotationFeature } from 'apollo-mst'
-import { Instance, SnapshotIn, types } from 'mobx-state-tree'
+import { AnnotationFeature, AnnotationFeatureI } from 'apollo-mst'
+import { autorun } from 'mobx'
+import { Instance, SnapshotIn, addDisposer, types } from 'mobx-state-tree'
 
-export const ApolloFeatureDetailsWidgetModel = types.model(
-  'ApolloFeatureDetailsWidget',
-  {
+import { ApolloSessionModel } from '../session'
+
+export const ApolloFeatureDetailsWidgetModel = types
+  .model('ApolloFeatureDetailsWidget', {
     id: ElementId,
     type: types.literal('ApolloFeatureDetailsWidget'),
-    feature: types.safeReference(AnnotationFeature),
+    feature: types.maybe(
+      types.reference(AnnotationFeature, {
+        onInvalidated(ev) {
+          ev.parent.setTryReload(ev.invalidId)
+          ev.removeRef()
+        },
+      }),
+    ),
     assembly: types.string,
     refName: types.string,
-  },
-)
+  })
+  .volatile(() => ({
+    tryReload: undefined as string | undefined,
+  }))
+  .actions((self) => ({
+    setFeature(feature: AnnotationFeatureI) {
+      self.feature = feature
+    },
+    setTryReload(featureId?: string) {
+      self.tryReload = featureId
+    },
+  }))
+  .actions((self) => ({
+    afterAttach() {
+      addDisposer(
+        self,
+        autorun((reaction) => {
+          if (!self.tryReload) {
+            return
+          }
+          const session = getSession(self) as unknown as ApolloSessionModel
+          const { apolloDataStore } = session
+          if (!apolloDataStore) {
+            return
+          }
+          const feature = apolloDataStore.getFeature(self.tryReload)
+          if (feature) {
+            self.setFeature(feature)
+            self.setTryReload()
+            reaction.dispose()
+          }
+        }),
+      )
+    },
+  }))
 
 export type ApolloFeatureDetailsWidget = Instance<
   typeof ApolloFeatureDetailsWidgetModel
