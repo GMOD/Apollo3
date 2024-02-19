2581dc0a5ed51f8b64797203546401c878bf6789
dariober <dario.beraldi@gmail.com>
2024-02-19 15:25:04 +0000

    Test hook

--- packages/apollo-cli/src/commands/assemblies/get.ts
+++ packages/apollo-cli/src/commands/assemblies/get.ts
@@ -4,7 +4,7 @@ import { BaseCommand } from '../../baseCommand.js'
 import { filterJsonList, queryApollo } from '../../utils.js'
 
 export default class Get extends BaseCommand<typeof Get> {
-  static description = 'Get available assemblies'
+  static description = 'Get available assemblies <testme>'
 
   static flags = {
     names: Flags.string({
