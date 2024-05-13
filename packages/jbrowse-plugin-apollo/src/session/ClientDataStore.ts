75efe5a584f6fd5d8ae79001f2fb1b5e1d5694ff
Garrett Stevens <stevens.garrett.j@gmail.com>
2024-05-13 03:22:44 +0000

    Keep local GFF features when refreshing page

--- packages/jbrowse-plugin-apollo/src/session/ClientDataStore.ts
+++ packages/jbrowse-plugin-apollo/src/session/ClientDataStore.ts
@@ -8,7 +8,9 @@ import {
   AnnotationFeatureModelNew,
   AnnotationFeatureSnapshotNew,
   ApolloAssembly,
+  ApolloAssemblySnapshot,
   ApolloRefSeq,
+  BackendDriverType,
   CheckResult,
   CheckResultSnapshot,
 } from '@apollo-annotation/mst'
@@ -69,8 +71,15 @@ export function clientDataStoreFactory(
       },
     }))
     .actions((self) => ({
-      addAssembly(assemblyId: string) {
-        return self.assemblies.put({ _id: assemblyId, refSeqs: {} })
+      addAssembly(assemblyId: string, backendDriverType?: BackendDriverType) {
+        const assemblySnapshot: ApolloAssemblySnapshot = {
+          _id: assemblyId,
+          refSeqs: {},
+        }
+        if (backendDriverType) {
+          assemblySnapshot.backendDriverType = backendDriverType
+        }
+        return self.assemblies.put(assemblySnapshot)
       },
       addFeature(assemblyId: string, feature: AnnotationFeatureSnapshotNew) {
         const assembly = self.assemblies.get(assemblyId)
