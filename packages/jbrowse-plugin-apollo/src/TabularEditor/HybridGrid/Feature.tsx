37480eb1ebba512f486dc15b8b90e148bb3d3b2e
Garrett Stevens <stevens.garrett.j@gmail.com>
2024-07-10 01:18:59 +0000

    Don't  use prettier in eslint

--- packages/jbrowse-plugin-apollo/src/TabularEditor/HybridGrid/Feature.tsx
+++ packages/jbrowse-plugin-apollo/src/TabularEditor/HybridGrid/Feature.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/no-nested-ternary */
 /* eslint-disable @typescript-eslint/unbound-method */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
