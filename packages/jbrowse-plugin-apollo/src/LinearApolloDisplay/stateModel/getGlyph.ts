eb8a342dcd548d5dec1e214658f41e2d2cb96c19
Garrett Stevens <stevens.garrett.j@gmail.com>
2024-08-02 17:40:50 +0000

    CanonicalGeneGlyph -> GeneGlyph

--- packages/jbrowse-plugin-apollo/src/LinearApolloDisplay/stateModel/getGlyph.ts
+++ packages/jbrowse-plugin-apollo/src/LinearApolloDisplay/stateModel/getGlyph.ts
@@ -1,23 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { AnnotationFeature } from '@apollo-annotation/mst'
 
-import {
-  BoxGlyph,
-  CanonicalGeneGlyph,
-  GenericChildGlyph,
-} from '../glyphs'
+import { BoxGlyph, GeneGlyph, GenericChildGlyph } from '../glyphs'
 import { Glyph } from '../glyphs/Glyph'
 
 const boxGlyph = new BoxGlyph()
-const canonicalGeneGlyph = new CanonicalGeneGlyph()
+const geneGlyph = new GeneGlyph()
 const genericChildGlyph = new GenericChildGlyph()
 
 /** get the appropriate glyph for the given top-level feature */
 export function getGlyph(feature: AnnotationFeature, _bpPerPx: number): Glyph {
   if (feature.type === 'gene') {
-
-      return canonicalGeneGlyph
-
+    return geneGlyph
   }
   if (feature.children?.size) {
     return genericChildGlyph
