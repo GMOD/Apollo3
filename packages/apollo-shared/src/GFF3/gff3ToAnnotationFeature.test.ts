823092746ff973d77e6987ceb74a74eb6bf9d1bb
dariober <dario.beraldi@gmail.com>
2024-09-04 09:20:37 +0100

    Some working tests

--- packages/apollo-shared/src/GFF3/gff3ToAnnotationFeature.test.ts
+++ packages/apollo-shared/src/GFF3/gff3ToAnnotationFeature.test.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { describe, it } from 'node:test'
 import { readFileSync } from 'node:fs'
@@ -97,65 +94,6 @@ function readSingleFeatureFile(fn: string): GFF3Feature {
   return inGff[0]
 }
 
-function renameIds(x: AnnotationFeatureSnapshot): AnnotationFeatureSnapshot {
-  const gene = JSON.parse(JSON.stringify(x))
-  if (gene.children) {
-    for (const mrnaId of Object.keys(gene.children)) {
-      const mrnaNewId = gene.children[mrnaId].attributes?.testid
-      if (!mrnaNewId) {
-        throw new Error('Expected to find testid')
-      }
-      const mrna = gene.children[mrnaId]
-      gene.children[mrnaNewId] = mrna
-      delete gene.children[mrnaId]
-      if (mrna.children) {
-        for (const exonId of Object.keys(mrna.children)) {
-          const exonNewId = mrna.children[exonId].attributes?.testid
-          if (!exonNewId) {
-            throw new Error('Expected to find testid')
-          }
-          const exon = gene.children[mrnaNewId].children[exonId]
-          gene.children[mrnaNewId].children[exonNewId] = exon
-          delete gene.children[mrnaNewId].children[exonId]
-          if (exon.children) {
-            for (const cdsId of Object.keys(exon.children)) {
-              const cdsNewId = exon.children[cdsId].attributes?.testid
-              if (!cdsNewId) {
-                throw new Error('Expected to find testid')
-              }
-              const cds =
-                gene.children[mrnaNewId].children[exonNewId].children[cdsId]
-              gene.children[mrnaNewId].children[exonNewId].children[cdsNewId] =
-                cds
-              delete gene.children[mrnaNewId].children[exonNewId].children[
-                cdsId
-              ]
-              if (cds.children) {
-                for (const subId of Object.keys(cds.children)) {
-                  const subNewId = cds.children[subId].attributes?.testid
-                  if (!subNewId) {
-                    throw new Error('Expected to find testid')
-                  }
-                  const sub =
-                    gene.children[mrnaNewId].children[exonNewId].children[
-                      cdsNewId
-                    ].children[subId]
-                  gene.children[mrnaNewId].children[exonNewId].children[
-                    cdsNewId
-                  ].children[subNewId] = sub
-                  delete gene.children[mrnaNewId].children[exonNewId].children[
-                    cdsNewId
-                  ].children[subId]
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  return gene as AnnotationFeatureSnapshot
-}
 interface AnnotationFeatureSnapshotWithChildrenArray
   extends Omit<AnnotationFeatureSnapshot, 'children'> {
   children?: AnnotationFeatureSnapshotWithChildrenArray[]
@@ -185,99 +123,51 @@ function compareFeatures(
   )
 }
 
-describe('gff3ToAnnotationFeature', () => {
-  for (const testCase of testCases) {
-    const [description, featureLine, convertedFeature] = testCase
-    it(`converts ${description}`, () => {
-      const gff3Feature = gff.parseStringSync(featureLine, {
-        parseSequences: false,
-      })
-      const feature = gff3ToAnnotationFeature(gff3Feature[0])
-      compareFeatures(convertedFeature, feature)
-    })
-  }
+function readAnnotationFeatureSnapshot(fn: string): AnnotationFeatureSnapshot {
+  const lines = readFileSync(fn).toString()
+  return JSON.parse(lines) as AnnotationFeatureSnapshot
+}
 
-  it.skip('converts gene with mRNA', () => {
-    const expected: AnnotationFeatureSnapshot = {
-      _id: '66cc92b3f580b24ed78564b5',
-      refSeq: 'ctgA',
-      type: 'gene',
-      min: 999,
-      max: 2000,
-      children: {
-        '66cc92b3f580b24ed78564b6': {
-          _id: '66cc92b3f580b24ed78564b6',
-          refSeq: 'ctgA',
-          type: 'mRNA',
-          min: 999,
-          max: 2000,
-          attributes: {
-            gff_source: ['example'],
-            gff_name: ['mrnaA'],
-            gff_alias: ['hga'],
-          },
-        },
-      },
-      attributes: {
-        gff_source: ['example'],
-        gff_id: ['gene01'],
-        gff_name: ['geneA'],
-        gff_alias: ['hga'],
-      },
-    }
-
-    const feature: GFF3Feature = readSingleFeatureFile(
-      'test_data/gene_mrna.gff3',
+describe('gff3ToAnnotationFeature examples', () => {
+  it('Convert one CDS', () => {
+    const actual = gff3ToAnnotationFeature(
+      readSingleFeatureFile('test_data/one_cds.gff3'),
     )
-    const actual = gff3ToAnnotationFeature(feature)
+    const expected = readAnnotationFeatureSnapshot('test_data/one_cds.json')
     compareFeatures(actual, expected)
   })
-
-  it.only('converts example', () => {
-    const _ex1 = gff3ToAnnotationFeature(
+  it('Convert two CDSs', () => {
+    const actual = gff3ToAnnotationFeature(
+      readSingleFeatureFile('test_data/two_cds.gff3'),
+    )
+    const expected = readAnnotationFeatureSnapshot('test_data/two_cds.json')
+    compareFeatures(actual, expected)
+  })
+  it('Convert example 1', () => {
+    const actual = gff3ToAnnotationFeature(
       readSingleFeatureFile('test_data/example01.gff3'),
     )
-    console.log(JSON.stringify(_ex1, null, 2))
-
-    const ex1 = renameIds(_ex1)
-
-    const _ex2 = gff3ToAnnotationFeature(
+    const expected = readAnnotationFeatureSnapshot('test_data/example01.json')
+    compareFeatures(actual, expected)
+  })
+  it('Convert example 2', () => {
+    const actual = gff3ToAnnotationFeature(
       readSingleFeatureFile('test_data/example02.gff3'),
     )
-    const ex2 = renameIds(_ex2)
-
-    const gene1 = structuredClone(ex1)
-    gene1.children = undefined
-    const gene2 = structuredClone(ex2)
-    gene2.children = undefined
-    compareFeatures(gene1, gene2)
-
-    assert.ok(ex1.children)
-    assert.ok(ex2.children)
-
-    assert.equal(
-      JSON.stringify(Object.keys(ex1.children).sort()),
-      JSON.stringify(['t004', 't005', 't006', 'x001']),
-    )
-    assert.equal(
-      JSON.stringify(Object.keys(ex2.children).sort()),
-      JSON.stringify(['t004', 't005', 't006']),
-    )
-
-    const mrna_1 = structuredClone(ex1).children?.t004
-    const mrna_2 = structuredClone(ex2).children?.t004
-
-    console.log(JSON.stringify(mrna_1, null, 2))
-
-    assert.ok(mrna_1?.children)
-    assert.ok(mrna_2?.children)
-
-    assert.equal(
-      Object.keys(mrna_1.children).length,
-      Object.keys(mrna_2.children).length,
-    )
-
-    compareFeatures(mrna_1.children.t007, mrna_2.children.t007)
-    compareFeatures(mrna_1.children.t015, mrna_2.children.t021)
+    const expected = readAnnotationFeatureSnapshot('test_data/example02.json')
+    compareFeatures(actual, expected)
   })
 })
+
+// describe('gff3ToAnnotationFeature', () => {
+//   for (const testCase of testCases) {
+//     const [description, featureLine, convertedFeature] = testCase
+//     it(`converts ${description}`, () => {
+//       const gff3Feature = gff.parseStringSync(featureLine, {
+//         parseSequences: false,
+//       })
+//       const feature = gff3ToAnnotationFeature(gff3Feature[0])
+//       compareFeatures(convertedFeature, feature)
+//     })
+//   }
+// })
