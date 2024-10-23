20ed2cf62cbc100a554fb916349871e25ba49ba7
Garrett Stevens <stevens.garrett.j@gmail.com>
2024-10-23 23:33:55 +0000

    Make feature type ontology configurable

--- packages/jbrowse-plugin-apollo/src/config.ts
+++ packages/jbrowse-plugin-apollo/src/config.ts
@@ -5,6 +5,11 @@ import { OntologyRecordConfiguration } from './OntologyManager'
 
 const ApolloPluginConfigurationSchema = ConfigurationSchema('ApolloPlugin', {
   ontologies: types.array(OntologyRecordConfiguration),
+  featureTypeOntologyName: {
+    description: 'Name of the feature type ontology',
+    type: 'string',
+    defaultValue: 'Sequence Ontology',
+  },
 })
 
 export default ApolloPluginConfigurationSchema
