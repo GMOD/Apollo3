1644cecf83fb1c32317197c3427ec71d991aa304
Garrett Stevens <stevens.garrett.j@gmail.com>
2024-11-15 17:01:40 -0700

    eslint packages minor version updates

--- packages/jbrowse-plugin-apollo/src/components/AddFeature.tsx
+++ packages/jbrowse-plugin-apollo/src/components/AddFeature.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { AddFeatureChange } from '@apollo-annotation/shared'
