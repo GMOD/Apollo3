8ed8e3f2d701a84069ed31fc769b856f68472016
dariober <dario.beraldi@gmail.com>
2024-12-02 14:08:33 +0000

    Process CDSs, tests extended

--- packages/apollo-shared/src/GFF3/annotationFeatureToGFF3.test.ts
+++ packages/apollo-shared/src/GFF3/annotationFeatureToGFF3.test.ts
@@ -4,9 +4,10 @@ import { describe, it } from 'node:test'
 import { assert } from 'chai'
 import { readAnnotationFeatureSnapshot } from './gff3ToAnnotationFeature.test'
 import { annotationFeatureToGFF3 } from './annotationFeatureToGFF3'
+import { AnnotationFeatureSnapshot } from '@apollo-annotation/mst'
 
 describe('annotationFeatureToGFF3', () => {
-  it('Convert one gene test fields', () => {
+  it('Test mandatory columns', () => {
     const annotationFeature = readAnnotationFeatureSnapshot(
       'test_data/gene.json',
     )
@@ -20,14 +21,74 @@ describe('annotationFeatureToGFF3', () => {
     assert.deepEqual(gff3Feature.score, 123)
     assert.deepEqual(gff3Feature.source, 'test_data')
   })
-  it.skip('Convert one gene test phase', () => {
-    const annotationFeature = readAnnotationFeatureSnapshot(
-      'test_data/gene.json',
-    )
+  it('Feature with no children and no gff_id has no ID attribute', () => {
+    const annotationFeature = JSON.parse(`{
+      "_id": "66d70e4ccc30b55b65e5f619",
+      "refSeq": "chr1",
+      "type": "gene",
+      "min": 999,
+      "max": 9000,
+      "strand": 1,
+      "attributes": {}
+    }`) as AnnotationFeatureSnapshot
     const [gff3Feature] = annotationFeatureToGFF3(annotationFeature)
-    const cds = gff3Feature.child_features[0][0].child_features[2][0]
-    assert.deepEqual(cds.start, 1201)
-    assert.deepEqual(cds.phase, '0')
+    assert.isUndefined(gff3Feature.attributes?.ID)
+  })
+  it('Feature with children and no gff_id has internal _id as ID', () => {
+    const annotationFeature = JSON.parse(`{
+      "_id": "66d70e4ccc30b55b65e5f619",
+      "refSeq": "chr1",
+      "type": "gene",
+      "min": 999,
+      "max": 9000,
+      "strand": 1,
+      "attributes": {},
+      "children": {
+        "66d70e4ccc30b55b65e5f618": {
+          "_id": "66d70e4ccc30b55b65e5f618",
+          "refSeq": "chr1",
+          "type": "gene_segment",
+          "min": 1049,
+          "max": 9000,
+          "strand": 1,
+          "attributes": {}
+        }
+      }
+    }`) as AnnotationFeatureSnapshot
+    const [gff3Feature] = annotationFeatureToGFF3(annotationFeature)
+    assert.deepEqual(gff3Feature.attributes?.ID, ['66d70e4ccc30b55b65e5f619'])
+  })
+  it('Convert multiple scores', () => {
+    const annotationFeature = JSON.parse(`{
+      "_id": "66d70e4ccc30b55b65e5f619",
+      "refSeq": "chr1",
+      "type": "gene",
+      "min": 999,
+      "max": 9000,
+      "strand": 1,
+      "attributes": {
+        "gff_id": ["gene10001"],
+        "gff_score": ["123", "345"]
+      }
+    }`) as AnnotationFeatureSnapshot
+    const [gff3Feature] = annotationFeatureToGFF3(annotationFeature)
+    assert.deepEqual(gff3Feature.score, 123)
+  })
+  it('Convert invalid score', () => {
+    const annotationFeature = JSON.parse(`{
+      "_id": "66d70e4ccc30b55b65e5f619",
+      "refSeq": "chr1",
+      "type": "gene",
+      "min": 999,
+      "max": 9000,
+      "strand": 1,
+      "attributes": {
+        "gff_id": ["gene10001"],
+        "gff_score": ["xyz"]
+      }
+    }`) as AnnotationFeatureSnapshot
+    const [gff3Feature] = annotationFeatureToGFF3(annotationFeature)
+    assert.deepEqual(gff3Feature.score, null)
   })
   it('Convert one gene test attributes', () => {
     const annotationFeature = readAnnotationFeatureSnapshot(
@@ -65,5 +126,45 @@ describe('annotationFeatureToGFF3', () => {
     assert.deepEqual(cds.type, 'CDS')
     assert.deepEqual(cds.attributes?.ID, ['cds10001'])
     assert.deepEqual(cds.attributes?.Parent, ['mRNA10001'])
+  })
+  it('Convert CDSs', () => {
+    const annotationFeature = readAnnotationFeatureSnapshot(
+      'test_data/gene.json',
+    )
+    const [gff3Feature] = annotationFeatureToGFF3(annotationFeature)
+    const [children] = gff3Feature.child_features
+    const [mrna] = children
+    const cds10001 = mrna.child_features.filter((child) => {
+      const id = child[0].attributes?.ID
+      return id !== undefined && id[0] === 'cds10001'
+    })
+    assert.deepEqual(cds10001.length, 2)
+
+    const cds1_1 = cds10001[0][0]
+    assert.deepEqual(cds1_1.attributes?.ID, ['cds10001'])
+    assert.deepEqual(cds1_1.start, 1201)
+    assert.deepEqual(cds1_1.end, 1500)
+    assert.deepEqual(cds1_1.phase, '0')
+
+    const cds1_2 = cds10001[1][0]
+    assert.deepEqual(cds1_2.attributes?.ID, ['cds10001'])
+    assert.deepEqual(cds1_2.start, 5000)
+    assert.deepEqual(cds1_2.end, 5100)
+    assert.deepEqual(cds1_2.phase, '0')
+
+    assert.deepEqual(cds1_1.child_features[0][0].attributes?.ID, [
+      'cds_region10001',
+    ])
+    assert.deepEqual(cds1_1.child_features[0][0].start, 1351)
+    assert.deepEqual(cds1_1.child_features[0][0].end, 1400)
+    assert.deepEqual(cds1_1.child_features[0][0].phase, null)
+
+    const cds10004 = mrna.child_features.filter((child) => {
+      const id = child[0].attributes?.ID
+      return id !== undefined && id[0] === 'cds10004'
+    })
+    assert.deepEqual(cds10004.length, 2)
+    const cds4_1 = cds10004[0][0]
+    assert.deepEqual(cds4_1.attributes?.ID, ['cds10004'])
   })
 })
--- packages/apollo-shared/src/GFF3/annotationFeatureToGFF3.ts
+++ packages/apollo-shared/src/GFF3/annotationFeatureToGFF3.ts
@@ -1,22 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-import { AnnotationFeatureSnapshot } from '@apollo-annotation/mst'
+import {
+  AnnotationFeatureSnapshot,
+  TranscriptPartLocation,
+  TranscriptPartNonCoding,
+} from '@apollo-annotation/mst'
 import { GFF3Feature } from '@gmod/gff'
+import { intersection2 } from '@jbrowse/core/util'
 
 export function annotationFeatureToGFF3(
   feature: AnnotationFeatureSnapshot,
   parentId?: string,
   refSeqNames?: Record<string, string | undefined>,
 ): GFF3Feature {
-  const locations = [{ start: feature.min, end: feature.max }]
-  // const locations = feature.discontinuousLocations?.length
-  //   ? feature.discontinuousLocations
-  //   : [{ start: feature.start, end: feature.end, phase: feature.phase }]
   const attributes: Record<string, string[] | undefined> = JSON.parse(
     JSON.stringify(feature.attributes),
   )
   const ontologyTerms: string[] = []
   const source = feature.attributes?.gff_source?.[0] ?? null
+
   delete attributes.gff_source
   if (parentId) {
     attributes.Parent = [parentId]
@@ -24,6 +26,8 @@ export function annotationFeatureToGFF3(
   if (attributes.gff_id) {
     attributes.ID = attributes.gff_id
     delete attributes.gff_id
+  } else if (feature.children) {
+    attributes.ID = [feature._id]
   }
   if (attributes.gff_name) {
     attributes.Name = attributes.gff_name
@@ -74,39 +78,180 @@ export function annotationFeatureToGFF3(
   }
 
   const gff_score = feature.attributes?.gff_score
-  let score = null
-  if (gff_score) {
-    if (gff_score.length == 1) {
+  let score: number | null = null
+  if (gff_score && gff_score.length > 0) {
+    if (gff_score[0]) {
       score = Number(gff_score[0])
-    } else {
-      throw new Error('Unexpected score')
+      if (Number.isNaN(score)) {
+        score = null
+      }
     }
+    delete attributes.gff_score
   }
-  delete attributes.gff_score
+
+  const locations = [{ start: feature.min, end: feature.max }]
 
   return locations.map((location) => ({
-    start: location.start + 1,
-    end: location.end,
+    start: Number(location.start) + 1,
+    end: Number(location.end),
     seq_id: refSeqNames ? refSeqNames[feature.refSeq] ?? null : feature.refSeq,
     source,
     type: feature.type,
     score,
     strand: feature.strand ? (feature.strand === 1 ? '+' : '-') : null,
     phase: null,
-    // phase:
-    //   location.phase === 0
-    //     ? '0'
-    //     : location.phase === 1
-    //       ? '1'
-    //       : location.phase === 2
-    //         ? '2'
-    //         : null,
     attributes: Object.keys(attributes).length > 0 ? attributes : null,
     derived_features: [],
-    child_features: feature.children
-      ? Object.values(feature.children).map((child) =>
-          annotationFeatureToGFF3(child, attributes.ID?.[0], refSeqNames),
-        )
-      : [],
+    child_features: prepareChildFeatures(
+      feature,
+      attributes.ID?.[0],
+      refSeqNames,
+    ),
   }))
+}
+
+function prepareChildFeatures(
+  feature: AnnotationFeatureSnapshot,
+  parentID?: string,
+  refSeqNames?: Record<string, string | undefined>,
+): GFF3Feature[] {
+  if (!feature.children) {
+    return []
+  }
+  if (feature.type === 'mRNA') {
+    const child_features: GFF3Feature[] = []
+    const cdsLocations = getCdsLocations(feature)
+    let cds_idx = 0
+    for (const child of Object.values(feature.children)) {
+      const gffChild = annotationFeatureToGFF3(child, parentID, refSeqNames)
+      if (child.type === 'CDS') {
+        for (const loc of cdsLocations[cds_idx]) {
+          const gffCds = JSON.parse(JSON.stringify(gffChild)) as GFF3Feature
+          if (gffCds.length != 1) {
+            // Do we need this check?
+            throw new Error(
+              `Unexpected CDS: ${JSON.stringify(gffCds, null, 2)}`,
+            )
+          }
+          gffCds[0].start = loc.min + 1
+          gffCds[0].end = loc.max
+          gffCds[0].phase = loc.phase.toString()
+          gffCds[0].type = loc.type // Do we need this?
+          child_features.push(gffCds)
+        }
+        cds_idx++
+      } else {
+        child_features.push(gffChild)
+      }
+    }
+    return child_features
+  }
+  return Object.values(feature.children).map((child) =>
+    annotationFeatureToGFF3(child, parentID, refSeqNames),
+  )
+}
+
+interface TranscriptPartCoding extends TranscriptPartLocation {
+  type: 'CDS'
+  phase: 0 | 1 | 2
+}
+type TranscriptPart = TranscriptPartCoding | TranscriptPartNonCoding
+type TranscriptParts = TranscriptPart[]
+
+function getTranscriptParts(
+  feature: AnnotationFeatureSnapshot,
+): TranscriptParts[] {
+  if (feature.type !== 'mRNA') {
+    throw new Error(
+      'Only features of type "mRNA" or equivalent can calculate CDS locations',
+    )
+  }
+  if (!feature.children) {
+    throw new Error('no CDS or exons in mRNA')
+  }
+  // In AnnotationFeatureModel we have `children.values()`
+  const children = Object.values(feature.children)
+  const cdsChildren = children.filter((child) => child.type === 'CDS')
+  if (cdsChildren.length === 0) {
+    throw new Error('no CDS in mRNA')
+  }
+  const transcriptParts: TranscriptParts[] = []
+  for (const cds of cdsChildren) {
+    const { max: cdsMax, min: cdsMin } = cds
+    const parts: TranscriptParts = []
+    let hasIntersected = false
+    const exonLocations: TranscriptPartLocation[] = []
+    for (const child of children) {
+      if (child.type === 'exon') {
+        exonLocations.push({ min: child.min, max: child.max })
+      }
+    }
+    exonLocations.sort(({ min: a }, { min: b }) => a - b)
+    for (const child of exonLocations) {
+      const lastPart = parts.at(-1)
+      if (lastPart) {
+        parts.push({ min: lastPart.max, max: child.min, type: 'intron' })
+      }
+      const [start, end] = intersection2(cdsMin, cdsMax, child.min, child.max)
+      let utrType: 'fivePrimeUTR' | 'threePrimeUTR'
+      if (hasIntersected) {
+        utrType = feature.strand === 1 ? 'threePrimeUTR' : 'fivePrimeUTR'
+      } else {
+        utrType = feature.strand === 1 ? 'fivePrimeUTR' : 'threePrimeUTR'
+      }
+      if (start !== undefined && end !== undefined) {
+        hasIntersected = true
+        if (start === child.min && end === child.max) {
+          parts.push({ min: start, max: end, phase: 0, type: 'CDS' })
+        } else if (start === child.min) {
+          parts.push(
+            { min: start, max: end, phase: 0, type: 'CDS' },
+            { min: end, max: child.max, type: utrType },
+          )
+        } else if (end === child.max) {
+          parts.push(
+            { min: child.min, max: start, type: utrType },
+            { min: start, max: end, phase: 0, type: 'CDS' },
+          )
+        } else {
+          parts.push(
+            { min: child.min, max: start, type: utrType },
+            { min: start, max: end, phase: 0, type: 'CDS' },
+            {
+              min: end,
+              max: child.max,
+              type:
+                utrType === 'fivePrimeUTR' ? 'threePrimeUTR' : 'fivePrimeUTR',
+            },
+          )
+        }
+      } else {
+        parts.push({ min: child.min, max: child.max, type: utrType })
+      }
+    }
+    parts.sort(({ min: a }, { min: b }) => a - b)
+    if (feature.strand === -1) {
+      parts.reverse()
+    }
+    let nextPhase: 0 | 1 | 2 = 0
+    const phasedParts = parts.map((loc) => {
+      if (loc.type !== 'CDS') {
+        return loc
+      }
+      const phase = nextPhase
+      nextPhase = ((3 - ((loc.max - loc.min - phase + 3) % 3)) % 3) as 0 | 1 | 2
+      return { ...loc, phase }
+    })
+    transcriptParts.push(phasedParts)
+  }
+  return transcriptParts
+}
+
+function getCdsLocations(
+  feature: AnnotationFeatureSnapshot,
+): TranscriptPartCoding[][] {
+  const transcriptParts = getTranscriptParts(feature)
+  return transcriptParts.map((transcript) =>
+    transcript.filter((transcriptPart) => transcriptPart.type === 'CDS'),
+  )
 }
