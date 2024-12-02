460d2007a4668cf10162ec3db3a4ebf9a2eb102e
Garrett Stevens <stevens.garrett.j@gmail.com>
2024-12-02 14:55:47 -0700

    Add email form to website

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -1,5 +1,6 @@
 // import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Head from '@docusaurus/Head'
 // import HomepageFeatures from '@site/src/components/HomepageFeatures'
 import Heading from '@theme/Heading'
 import Layout from '@theme/Layout'
@@ -33,11 +34,24 @@ function HomepageHeader() {
 export default function Home(): React.JSX.Element {
   const { siteConfig } = useDocusaurusContext()
   return (
-    <Layout title={siteConfig.title} description={siteConfig.tagline}>
-      <HomepageHeader />
-      {/* <main>
-        <HomepageFeatures />
-      </main> */}
-    </Layout>
+    <>
+      <Head>
+        <script>
+          {`(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+.push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+ml('account', '770673');
+`}
+        </script>
+      </Head>
+      <Layout title={siteConfig.title} description={siteConfig.tagline}>
+        <HomepageHeader />
+        <main>
+          <div className="ml-embedded" data-form="fAN2W8"></div>
+          {/* <HomepageFeatures /> */}
+        </main>
+      </Layout>
+    </>
   )
 }
