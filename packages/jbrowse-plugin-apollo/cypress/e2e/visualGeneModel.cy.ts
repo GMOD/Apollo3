61f1dbde38958173d73a76971685e75151f4f8cd
dariober <dario.beraldi@gmail.com>
2025-02-10 14:32:27 +0000

    Test for gene model with multiple exons and unusual SO terms

--- packages/jbrowse-plugin-apollo/cypress/e2e/visualGeneModel.cy.ts
+++ packages/jbrowse-plugin-apollo/cypress/e2e/visualGeneModel.cy.ts
@@ -6,18 +6,11 @@ describe('Simple tests for visuals', () => {
     cy.deleteAssemblies()
   })
   it('Shows correct gene model', () => {
-    cy.addAssemblyFromGff(
-      'stopcodon.gff3',
-      'test_data/cdsChecks/stopcodon.gff3',
-    )
-    cy.selectAssemblyToView('stopcodon.gff3')
-    cy.searchFeatures('gene08', 1)
+    cy.addAssemblyFromGff('so_types.gff3', 'test_data/so_types.gff3')
+    cy.selectAssemblyToView('so_types.gff3')
+    cy.searchFeatures('TGGT1_200010', 1)
     cy.wait(5000) // Wait for the gene model to render. It would be better to ensure some element of the canvas is actually there
 
     cy.get('canvas[data-testid="overlayCanvas"]').compareSnapshot('gene-model')
-
-    cy.get('canvas[data-testid="seqTrackOverlayCanvas"]').compareSnapshot(
-      'sixframe',
-    )
   })
 })
