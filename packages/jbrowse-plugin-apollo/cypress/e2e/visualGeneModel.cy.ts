b0a01e08e756f7cdf6d42fd7729e9e6e5bc3cb08
dariober <dario.beraldi@gmail.com>
2025-02-11 10:26:50 +0000

    Tmp commit: Wait for drawer to disappear, run only visual test

--- packages/jbrowse-plugin-apollo/cypress/e2e/visualGeneModel.cy.ts
+++ packages/jbrowse-plugin-apollo/cypress/e2e/visualGeneModel.cy.ts
@@ -10,10 +10,10 @@ describe('Simple tests for visuals', () => {
     cy.selectAssemblyToView('so_types.gff3')
     cy.searchFeatures('TGGT1_200010', 1)
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(5000) // Wait for the gene model to render. It would be better to ensure some element of the canvas is actually there
+    cy.wait(60_000) // Wait for the gene model to render. It would be better to ensure some element of the canvas is actually there
 
     // Wait for the Job list drwaer to disappear so we don't affect the size of the image
-    cy.get('input[value="JobList"]', { timeout: 60_000 }).should('not.exist')
+    // cy.get('input[value="JobList"]', { timeout: 60_000 }).should('not.exist')
 
     cy.get('canvas[data-testid="overlayCanvas"]').compareSnapshot('gene-model')
   })
