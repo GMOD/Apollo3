362dfd680ab291ade14b024fa2d2bb2404e31ff7
Dario Beraldi <dario.beraldi@gmail.com>
2025-04-16 05:46:03 +0200

    Add gene and transcript presets to "Add new feature" (#561)
    * Shortcuts to add gene or transcript with subfeatures (#556)
    
    * Show selected custom type
    
    * Add tootlips
    
    * Add sub-features in a single submission
    
    * Minor refactoring
    
    * Remove need for fallback text field
    
    ---------
    
    Co-authored-by: Garrett Stevens <stevens.garrett.j@gmail.com>

--- packages/jbrowse-plugin-apollo/src/components/AddFeature.tsx
+++ packages/jbrowse-plugin-apollo/src/components/AddFeature.tsx
@@ -4,17 +4,26 @@
 import { AddFeatureChange } from '@apollo-annotation/shared'
 import { AbstractSessionModel, Region } from '@jbrowse/core/util/types'
 import {
+  Box,
   Button,
   DialogActions,
   DialogContent,
   DialogContentText,
   FormControl,
+  FormControlLabel,
+  IconButton,
   InputLabel,
   MenuItem,
+  Radio,
+  RadioGroup,
   Select,
   SelectChangeEvent,
   TextField,
+  Tooltip,
 } from '@mui/material'
+
+import InfoIcon from '@mui/icons-material/Info'
+
 import ObjectID from 'bson-objectid'
 import React, { useState } from 'react'
 
@@ -23,12 +32,60 @@ import { isOntologyClass } from '../OntologyManager'
 import { ApolloSessionModel } from '../session'
 import { Dialog } from './Dialog'
 import { OntologyTermAutocomplete } from './OntologyTermAutocomplete'
+import { AnnotationFeatureSnapshot } from '@apollo-annotation/mst'
 
 interface AddFeatureProps {
   session: ApolloSessionModel
   handleClose(): void
   region: Region
   changeManager: ChangeManager
+}
+
+enum NewFeature {
+  GENE_AND_SUBFEATURES = 'GENE_AND_SUBFEATURES',
+  TRANSCRIPT_AND_SUBFEATURES = 'TRANSCRIPT_AND_SUBFEATURES',
+  CUSTOM = 'CUSTOM',
+}
+
+function makeCodingMrna(
+  refSeqId: string,
+  strand: 1 | -1 | undefined,
+  min: number,
+  max: number,
+): AnnotationFeatureSnapshot {
+  const cds = {
+    _id: new ObjectID().toHexString(),
+    refSeq: refSeqId,
+    type: 'CDS',
+    min,
+    max,
+    strand,
+  } as AnnotationFeatureSnapshot
+
+  const exon = {
+    _id: new ObjectID().toHexString(),
+    refSeq: refSeqId,
+    type: 'exon',
+    min,
+    max,
+    strand,
+  } as AnnotationFeatureSnapshot
+
+  const children: Record<string, AnnotationFeatureSnapshot> = {}
+  children[cds._id] = cds
+  children[exon._id] = exon
+
+  const mRNA = {
+    _id: new ObjectID().toHexString(),
+    refSeq: refSeqId,
+    type: 'mRNA',
+    min,
+    max,
+    strand,
+    children,
+  } as AnnotationFeatureSnapshot
+
+  return mRNA
 }
 
 export function AddFeature({
@@ -40,7 +97,8 @@ export function AddFeature({
   const { notify } = session as unknown as AbstractSessionModel
   const [end, setEnd] = useState(String(region.end))
   const [start, setStart] = useState(String(region.start + 1))
-  const [type, setType] = useState('')
+  const [type, setType] = useState<NewFeature>(NewFeature.GENE_AND_SUBFEATURES)
+  const [customType, setCustomType] = useState<string>()
   const [strand, setStrand] = useState<1 | -1 | undefined>()
   const [errorMessage, setErrorMessage] = useState('')
 
@@ -64,8 +122,53 @@ export function AddFeature({
       return
     }
 
+    let change
+    if (type === NewFeature.GENE_AND_SUBFEATURES) {
+      const mRNA = makeCodingMrna(
+        refSeqId,
+        strand,
+        Number(start) - 1,
+        Number(end),
+      )
+      const children: Record<string, AnnotationFeatureSnapshot> = {}
+      children[mRNA._id] = mRNA
+
+      const id = new ObjectID().toHexString()
+      change = new AddFeatureChange({
+        changedIds: [id],
+        typeName: 'AddFeatureChange',
+        assembly: region.assemblyName,
+        addedFeature: {
+          _id: id,
+          refSeq: refSeqId,
+          min: Number(start) - 1,
+          max: Number(end),
+          type: 'gene',
+          strand,
+          children,
+        },
+      })
+    } else if (type === NewFeature.TRANSCRIPT_AND_SUBFEATURES) {
+      const mRNA = makeCodingMrna(
+        refSeqId,
+        strand,
+        Number(start) - 1,
+        Number(end),
+      )
+      change = new AddFeatureChange({
+        changedIds: [mRNA._id],
+        typeName: 'AddFeatureChange',
+        assembly: region.assemblyName,
+        addedFeature: mRNA,
+      })
+    }
+
+    if (!customType) {
+      setErrorMessage('No type selected')
+      return
+    }
     const id = new ObjectID().toHexString()
-    const change = new AddFeatureChange({
+    change = new AddFeatureChange({
       changedIds: [id],
       typeName: 'AddFeatureChange',
       assembly: region.assemblyName,
@@ -74,19 +177,14 @@ export function AddFeature({
         refSeq: refSeqId,
         min: Number(start) - 1,
         max: Number(end),
-        type,
+        type: customType,
         strand,
       },
     })
     await changeManager.submit(change)
     notify('Feature added successfully', 'success')
     handleClose()
-    event.preventDefault()
-  }
-
-  function handleChangeType(newType: string) {
-    setErrorMessage('')
-    setType(newType)
+    return
   }
 
   function handleChangeStrand(e: SelectChangeEvent) {
@@ -109,6 +207,24 @@ export function AddFeature({
 
   const error = Number(end) <= Number(start)
 
+  function handleChangeOntologyType(newType: string) {
+    setErrorMessage('')
+    setCustomType(newType)
+  }
+
+  const handleTypeChange = (e: SelectChangeEvent) => {
+    setErrorMessage('')
+    const { value } = e.target
+    if (Object.keys(NewFeature).includes(value)) {
+      setType(NewFeature[value as NewFeature])
+    }
+  }
+
+  let submitDisabled: boolean = Boolean(error) || !(start && end && type)
+  if (type === NewFeature.CUSTOM && !customType) {
+    submitDisabled = true
+  }
+
   return (
     <Dialog
       open
@@ -117,7 +233,7 @@ export function AddFeature({
       maxWidth={false}
       data-testid="add-feature-dialog"
     >
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} data-testid="submit-form">
         <DialogContent style={{ display: 'flex', flexDirection: 'column' }}>
           <TextField
             margin="dense"
@@ -145,26 +261,6 @@ export function AddFeature({
             error={error}
             helperText={error ? '"End" must be greater than "Start"' : null}
           />
-          <OntologyTermAutocomplete
-            session={session}
-            ontologyName="Sequence Ontology"
-            style={{ width: 170 }}
-            value={type}
-            filterTerms={isOntologyClass}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Type"
-                variant="outlined"
-                fullWidth
-              />
-            )}
-            onChange={(oldValue, newValue) => {
-              if (newValue) {
-                handleChangeType(newValue)
-              }
-            }}
-          />
           <FormControl>
             <InputLabel id="demo-simple-select-label">Strand</InputLabel>
             <Select
@@ -179,13 +275,80 @@ export function AddFeature({
               <MenuItem value={-1}>-</MenuItem>
             </Select>
           </FormControl>
+
+          <FormControl style={{ marginTop: 20 }}>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue={NewFeature.GENE_AND_SUBFEATURES}
+              name="radio-buttons-group"
+              value={type}
+              onChange={handleTypeChange}
+            >
+              <FormControlLabel
+                value={NewFeature.GENE_AND_SUBFEATURES}
+                control={<Radio />}
+                label={
+                  <Box display="flex" alignItems="center">
+                    Add gene and sub-features
+                    <Tooltip title="This is a shortcut to create a gene with a single mRNA, exon, and CDS">
+                      <IconButton size="small">
+                        <InfoIcon sx={{ fontSize: 18 }} />
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
+                }
+              />
+
+              <FormControlLabel
+                value={NewFeature.TRANSCRIPT_AND_SUBFEATURES}
+                control={<Radio />}
+                label={
+                  <Box display="flex" alignItems="center">
+                    Add transcript and sub-features
+                    <Tooltip title="This is a shortcut to create a single mRNA with exon and CDS, but without a parent gene">
+                      <IconButton size="small">
+                        <InfoIcon sx={{ fontSize: 18 }} />
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
+                }
+              />
+              <FormControlLabel
+                value={NewFeature.CUSTOM}
+                checked={
+                  type !== NewFeature.GENE_AND_SUBFEATURES &&
+                  type !== NewFeature.TRANSCRIPT_AND_SUBFEATURES
+                }
+                control={<Radio />}
+                label="Add feature with with a sequence ontology type"
+              />
+            </RadioGroup>
+          </FormControl>
+          {type === NewFeature.CUSTOM ? (
+            <OntologyTermAutocomplete
+              session={session}
+              ontologyName="Sequence Ontology"
+              style={{ width: 170 }}
+              value=""
+              filterTerms={isOntologyClass}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Type"
+                  variant="outlined"
+                  fullWidth
+                />
+              )}
+              onChange={(_oldValue, newValue) => {
+                if (newValue) {
+                  handleChangeOntologyType(newValue)
+                }
+              }}
+            />
+          ) : null}
         </DialogContent>
         <DialogActions>
-          <Button
-            variant="contained"
-            type="submit"
-            disabled={error || !(start && end && type)}
-          >
+          <Button variant="contained" type="submit" disabled={submitDisabled}>
             Submit
           </Button>
           <Button variant="outlined" type="submit" onClick={handleClose}>
