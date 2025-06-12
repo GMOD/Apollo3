9ab2eeed1957775264e003453ffb1c05b3d55cf5
dariober <dario.beraldi@gmail.com>
2025-06-12 14:52:48 +0100

    Fix import

--- packages/apollo-shared/src/Changes/DeleteFeatureChange.ts
+++ packages/apollo-shared/src/Changes/DeleteFeatureChange.ts
@@ -10,9 +10,9 @@ import {
   type ServerDataStore,
 } from '@apollo-annotation/common'
 import { type AnnotationFeatureSnapshot } from '@apollo-annotation/mst'
+import { type Feature } from '@apollo-annotation/schemas'
 
 import { AddFeatureChange } from './AddFeatureChange'
-import { Feature } from '@apollo-annotation/schemas'
 
 interface SerializedDeleteFeatureChangeBase extends SerializedFeatureChange {
   typeName: 'DeleteFeatureChange'
