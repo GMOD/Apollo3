5917734ac422a5b030e8c11c62d9184890d9de1c
Garrett Stevens <stevens.garrett.j@gmail.com>
2025-07-23 00:26:24 +0000

    Add LinearApolloReferenceSequenceDisplay

---
+++ packages/jbrowse-plugin-apollo/src/LinearApolloReferenceSequenceDisplay/configSchema.ts
@@ -0,0 +1,7 @@
+import { ConfigurationSchema } from '@jbrowse/core/configuration'
+
+export const configSchema = ConfigurationSchema(
+  'LinearApolloReferenceSequenceDisplay',
+  {},
+  { explicitIdentifier: 'displayId', explicitlyTyped: true },
+)
