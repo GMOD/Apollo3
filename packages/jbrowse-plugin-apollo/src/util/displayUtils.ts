7693f1bccb53412526edc418166f4d7d3b8b6efc
Will Haese-Hill <wahhill@gmail.com>
2025-08-26 12:43:24 +0100

    Cluster check results to remove visual clutter (#664)
    * basic check result clustering with fixed width
    
    * simple fix to ensure clustering is dynamic with zoom level
    
    * generalise width param
    
    * fix to ensure check result clusters are rendered on correct row
    
    * move common functions to displayUtils
    
    * add mui badge with cluster number
    
    * fix tests by making bp start loc of ErrorIcon explicit
    
    ---------
    
    Co-authored-by: Garrett Stevens <stevens.garrett.j@gmail.com>

---
+++ packages/jbrowse-plugin-apollo/src/util/displayUtils.ts
@@ -0,0 +1,139 @@
+import { type CheckResultIdsType } from '@apollo-annotation/mst'
+import { makeStyles } from 'tss-react/mui'
+
+export type Coord = [number, number]
+
+export const useStyles = makeStyles()((theme) => ({
+  canvasContainer: {
+    position: 'relative',
+    left: 0,
+  },
+  canvas: {
+    position: 'absolute',
+    left: 0,
+  },
+  center: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  ellipses: {
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+  },
+  avatar: {
+    position: 'static',
+    height: '100%',
+    width: '100%',
+    overflow: 'visible',
+    color: theme.palette.warning.light,
+    backgroundColor: theme.palette.warning.contrastText,
+  },
+  box: {
+    position: 'absolute',
+    overflow: 'visible',
+  },
+  badge: {
+    display: 'inline-block',
+  },
+  loading: {
+    position: 'absolute',
+    right: theme.spacing(3),
+    zIndex: 10,
+    pointerEvents: 'none',
+    textAlign: 'right',
+  },
+}))
+
+export interface CheckResultCluster<T> {
+  _id: string
+  message: string
+  start: number
+  count: number
+  members: T[]
+  range: { min: number; max: number }
+  featureIds: CheckResultIdsType
+}
+
+export function clusterResultByMessage<
+  T extends {
+    _id: string
+    start: number
+    end: number
+    message: string
+    ids: CheckResultIdsType
+  },
+>(
+  items: readonly T[],
+  width: number,
+  touchesAsOverlap: boolean,
+): CheckResultCluster<T>[] {
+  const byMsg = new Map<string, T[]>()
+  for (const it of items) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    ;(byMsg.get(it.message) ?? byMsg.set(it.message, []).get(it.message)!).push(
+      it,
+    )
+  }
+
+  const clusters: CheckResultCluster<T>[] = []
+  const overlaps = (aEnd: number, bStart: number) =>
+    touchesAsOverlap ? bStart <= aEnd : bStart < aEnd
+
+  for (const [message, arr] of byMsg.entries()) {
+    if (arr.length === 0) {
+      continue
+    }
+
+    arr.sort((a, b) => a.start - b.start)
+
+    let group: T[] = [arr[0]]
+    let curMin = arr[0].start
+    let curMax = arr[0].start + width
+
+    const pushResult = () => {
+      const starts = group.map((d) => d.start).sort((a, b) => a - b)
+      const mid = Math.floor(starts.length / 2)
+      const median: number =
+        starts.length % 2 ? starts[mid] : (starts[mid - 1] + starts[mid]) / 2
+      const clusterId = group[0]._id
+      const featureIds = group[0].ids
+
+      clusters.push({
+        _id: clusterId,
+        message,
+        start: median,
+        count: group.length,
+        members: [...group],
+        range: { min: curMin, max: curMax },
+        featureIds,
+      })
+    }
+
+    for (let i = 1; i < arr.length; i++) {
+      const it = arr[i]
+      const itStart = it.start
+      const itEnd = itStart + width
+
+      if (overlaps(curMax, itStart)) {
+        group.push(it)
+        if (itStart < curMin) {
+          curMin = itStart
+        }
+        if (itEnd > curMax) {
+          curMax = itEnd
+        }
+      } else {
+        pushResult()
+        group = [it]
+        curMin = itStart
+        curMax = itEnd
+      }
+    }
+    pushResult()
+  }
+
+  clusters.sort(
+    (a, b) => a.message.localeCompare(b.message) || a.start - b.start,
+  )
+  return clusters
+}
