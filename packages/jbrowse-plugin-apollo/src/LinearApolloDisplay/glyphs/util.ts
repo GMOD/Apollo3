16fc87b4e6442f78e8a39463f2fb4f59aa563327
Garrett Stevens <stevens.garrett.j@gmail.com>
2025-11-05 17:52:53 +0000

    Refactor CDS drawing into CDSGlyph

--- packages/jbrowse-plugin-apollo/src/LinearApolloDisplay/glyphs/util.ts
+++ packages/jbrowse-plugin-apollo/src/LinearApolloDisplay/glyphs/util.ts
@@ -17,3 +17,20 @@ export function getLeftPx(
     featureLeftBpDistanceFromBlockLeftBp / bpPerPx
   return blockLeftPx + featureLeftPxDistanceFromBlockLeftPx
 }
+
+/**
+ * Perform a canvas strokeRect, but have the stroke be contained within the
+ * given rect instead of centered on it.
+ */
+export function strokeRectInner(
+  ctx: CanvasRenderingContext2D,
+  left: number,
+  top: number,
+  width: number,
+  height: number,
+  color: string,
+) {
+  ctx.strokeStyle = color
+  ctx.lineWidth = 1
+  ctx.strokeRect(left + 0.5, top + 0.5, width - 1, height - 1)
+}
