01c73d2969b5a7f98e070de003a82f201ae564cc
Garrett Stevens <stevens.garrett.j@gmail.com>
2025-11-10 23:24:50 +0000

    Small CheckResultWarnings optimizations

--- packages/jbrowse-plugin-apollo/src/LinearApolloDisplay/components/CheckResultWarnings.tsx
+++ packages/jbrowse-plugin-apollo/src/LinearApolloDisplay/components/CheckResultWarnings.tsx
@@ -6,6 +6,7 @@ import { observer } from 'mobx-react'
 import React from 'react'
 
 import { clusterResultByMessage, useStyles } from '../../util/displayUtils'
+import { getLeftPx } from '../glyphs/util'
 import { type LinearApolloDisplay as LinearApolloDisplayI } from '../stateModel'
 
 export const CheckResultWarnings = observer(function CheckResultWarnings({
@@ -16,79 +17,76 @@ export const CheckResultWarnings = observer(function CheckResultWarnings({
   const { classes } = useStyles()
   const { apolloDragging, apolloRowHeight, lgv, session, showCheckResults } =
     display
-  return lgv.displayedRegions.flatMap((region, idx) => {
+  const { assemblyManager } = session as unknown as AbstractSessionModel
+  if (!showCheckResults) {
+    return null
+  }
+  return lgv.dynamicBlocks.contentBlocks.map((block) => {
     const widthBp = lgv.bpPerPx * apolloRowHeight
-    const { assemblyManager } = session as unknown as AbstractSessionModel
-    const assembly = assemblyManager.get(region.assemblyName)
-    if (showCheckResults) {
-      const filteredCheckResults = [
-        ...session.apolloDataStore.checkResults.values(),
-      ].filter(
-        (checkResult) =>
-          assembly?.isValidRefName(checkResult.refSeq) &&
-          assembly.getCanonicalRefName(checkResult.refSeq) === region.refName &&
-          doesIntersect2(
-            region.start,
-            region.end,
-            checkResult.start,
-            checkResult.end,
-          ),
-      )
-      const checkResults = clusterResultByMessage<CheckResultI>(
-        filteredCheckResults,
-        widthBp,
-        true,
-      )
-      return checkResults.map((checkResult) => {
-        const left =
-          (lgv.bpToPx({
-            refName: region.refName,
-            coord: checkResult.start,
-            regionNumber: idx,
-          })?.offsetPx ?? 0) - lgv.offsetPx
-        const [feature] = checkResult.featureIds
-        if (!feature) {
-          return null
-        }
-        let row = 0
-        const featureLayout = display.getFeatureLayoutPosition(feature)
-        if (featureLayout) {
-          row = featureLayout.layoutRow + featureLayout.featureRow
-        }
-        const top = row * apolloRowHeight
-        const height = apolloRowHeight
-        return (
-          <Tooltip key={checkResult._id} title={checkResult.message}>
-            <Box
-              className={classes.box}
-              style={{
-                top,
-                left,
-                height,
-                width: height,
-                pointerEvents: apolloDragging ? 'none' : 'auto',
-              }}
-            >
-              <Badge
-                className={classes.badge}
-                badgeContent={checkResult.count}
-                color="primary"
-                overlap="circular"
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'right',
-                }}
-                invisible={checkResult.count <= 1}
-              >
-                <Avatar className={classes.avatar}>
-                  <ErrorIcon data-testid={`ErrorIcon-${checkResult.start}`} />
-                </Avatar>
-              </Badge>
-            </Box>
-          </Tooltip>
-        )
-      })
+    const assembly = assemblyManager.get(block.assemblyName)
+    if (!assembly) {
+      return null
     }
+    const filteredCheckResults = [
+      ...session.apolloDataStore.checkResults.values(),
+    ].filter(
+      (checkResult) =>
+        assembly.isValidRefName(checkResult.refSeq) &&
+        assembly.getCanonicalRefName(checkResult.refSeq) === block.refName &&
+        doesIntersect2(
+          block.start,
+          block.end,
+          checkResult.start,
+          checkResult.end,
+        ),
+    )
+    const checkResults = clusterResultByMessage<CheckResultI>(
+      filteredCheckResults,
+      widthBp,
+      true,
+    )
+    return checkResults.map((checkResult) => {
+      const left = Math.round(getLeftPx(display, checkResult.range, block))
+
+      const [feature] = checkResult.featureIds
+      if (!feature) {
+        return null
+      }
+      let row = 0
+      const featureLayout = display.getFeatureLayoutPosition(feature)
+      if (featureLayout) {
+        row = featureLayout.layoutRow + featureLayout.featureRow
+      }
+      const top = row * apolloRowHeight
+      const height = apolloRowHeight
+      return (
+        <Tooltip key={checkResult._id} title={checkResult.message}>
+          <Box
+            className={classes.box}
+            style={{
+              top,
+              left,
+              height,
+              width: height,
+              pointerEvents: apolloDragging ? 'none' : 'auto',
+            }}
+          >
+            <Badge
+              className={classes.badge}
+              badgeContent={checkResult.count}
+              color="primary"
+              overlap="circular"
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+              invisible={checkResult.count <= 1}
+            >
+              <Avatar className={classes.avatar}>
+                <ErrorIcon data-testid={`ErrorIcon-${checkResult.start}`} />
+              </Avatar>
+            </Badge>
+          </Box>
+        </Tooltip>
+      )
+    })
     return null
   })
 })
