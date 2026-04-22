8194fd7c1d84be7c1e5d3eeea1b2948acef1abf0
Shashank Budhanuru Ramaraju <shabr@ebi.ac.uk>
2026-04-22 14:07:02 +0100

    skip attributes before feature copy

--- packages/jbrowse-plugin-apollo/src/config.ts
+++ packages/jbrowse-plugin-apollo/src/config.ts
@@ -3,36 +3,42 @@ import { types } from '@jbrowse/mobx-state-tree'
 
 import { OntologyRecordConfiguration } from './OntologyManager'
 
-const ApolloPluginConfigurationSchema = ConfigurationSchema(
-  'ApolloPlugin',
-  {
-    ontologies: types.array(OntologyRecordConfiguration),
-    featureTypeOntologyName: {
-      description: 'Name of the feature type ontology',
-      type: 'string',
-      defaultValue: 'Sequence Ontology',
-    },
-    hasRole: {
-      description: 'Flag used internally by jbrowse-plugin-apollo',
-      type: 'boolean',
-      defaultValue: false,
-    },
-    geneBackgroundColor: {
-      description: 'Color for feature background',
-      type: 'string',
-      defaultValue: 'jexl:geneBackgroundColor(featureType)',
-      contextVariable: ['featureType'],
-    },
-  },
-  {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    actions: (self: any) => ({
-      addOntology(ontologySnapshot: { name: string }) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-        self.ontologies.push(ontologySnapshot)
+const ApolloPluginConfigurationSchema: ReturnType<typeof ConfigurationSchema> =
+  ConfigurationSchema(
+    'ApolloPlugin',
+    {
+      ontologies: types.array(OntologyRecordConfiguration),
+      featureTypeOntologyName: {
+        description: 'Name of the feature type ontology',
+        type: 'string',
+        defaultValue: 'Sequence Ontology',
       },
-    }),
-  },
-)
+      hasRole: {
+        description: 'Flag used internally by jbrowse-plugin-apollo',
+        type: 'boolean',
+        defaultValue: false,
+      },
+      skippedAttributesOnCopy: {
+        description: 'Feature attribute keys to skip when copying features',
+        type: 'stringArray',
+        defaultValue: [],
+      },
+      geneBackgroundColor: {
+        description: 'Color for feature background',
+        type: 'string',
+        defaultValue: 'jexl:geneBackgroundColor(featureType)',
+        contextVariable: ['featureType'],
+      },
+    },
+    {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      actions: (self: any) => ({
+        addOntology(ontologySnapshot: { name: string }) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+          self.ontologies.push(ontologySnapshot)
+        },
+      }),
+    },
+  )
 
 export default ApolloPluginConfigurationSchema
