9655e6811690cbf256f0cee8d2bb7e0bf2d4731d
Garrett Stevens <stevens.garrett.j@gmail.com>
2026-07-31 20:53:33 +0000

    Fix Jest under TS 6

--- packages/jbrowse-plugin-apollo/jest.config.cjs
+++ packages/jbrowse-plugin-apollo/jest.config.cjs
@@ -4,10 +4,17 @@
 
 const { createDefaultEsmPreset } = require('ts-jest')
 
+const defaultEsmPreset = createDefaultEsmPreset()
+// eslint-disable-next-line unicorn/prefer-string-raw
+defaultEsmPreset.transform['^.+\\.m?tsx?$'][1].tsconfig = {
+  module: 'nodenext',
+  moduleResolution: 'nodenext',
+}
+
 module.exports = {
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
   automock: false,
   setupFiles: ['./jestSetup.js', 'fake-indexeddb/auto'],
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
-  ...createDefaultEsmPreset(),
+  ...defaultEsmPreset,
 }
